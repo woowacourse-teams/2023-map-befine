3283798d37b2194fb6f58941214ae76f180192c4
jaeyeon kim <jakind@naver.com>
2023-07-13 17:20:34 +0900

    chore: 초기 환경 설정

---
+++ backend/src/main/java/com/mapbefine/mapbefine/MapbefineApplication.java
@@ -0,0 +1,13 @@
+package com.mapbefine.mapbefine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapbefineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapbefineApplication.class, args);
+	}
+
+}
