9fb0db41ff94b4009854022a95cb459268ccf017
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-07-17 14:18:47 +0900

    chore: 모듈 시스템 및 ts-parser 오류 해결

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,16 +1,30 @@
 module.exports = {
   extends: ['airbnb', 'prettier', 'plugin:storybook/recommended'],
   plugins: ['simple-import-sort'],
+  parser: '@typescript-eslint/parser',
   rules: {
     'import/extensions': ['off'],
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': ['error', {
-      extensions: ['.js', '.jsx', '.ts', '.tsx']
-    }],
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: true
-    }],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
     'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error'
-  }
+    'simple-import-sort/exports': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
