a5b38536089187643a2e9f13ef65a92e0958fe33
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-07-18 14:41:03 +0900

    feat: React Router 기능 구현 (#50)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import Home from './pages/Home';
+import PinDetail from './pages/PinDetail';
+import Topics from './pages/Topics';
 
 const App = () => {
   return (
     <>
       <Layout>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="topics/:topicId" element={<Topics />} />
+        </Routes>
       </Layout>
     </>
   );
