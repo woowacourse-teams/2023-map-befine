9f11c21a160dbbf35cf9059a29b820a976f8ec78
junpakPark <junpak.park@gmail.com>
2023-07-18 21:25:15 +0900

    feat: Coordinate의 위,경도가 대한민국 내에 있는 지 검증 기능 구현
    Co-authored-by: yoondgu <doyoungwork@gmail.com>

--- backend/src/main/java/com/mapbefine/mapbefine/entity/Coordinate.java
+++ backend/src/main/java/com/mapbefine/mapbefine/entity/Coordinate.java
@@ -1,9 +1,10 @@
 package com.mapbefine.mapbefine.entity;
 
-import static lombok.AccessLevel.PROTECTED;
+import static lombok.AccessLevel.*;
+
+import java.math.BigDecimal;
 
 import jakarta.persistence.Embeddable;
-import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,15 +13,33 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Coordinate {
 
-    private BigDecimal latitude;
-    private BigDecimal longitude;
+	private BigDecimal latitude;
+	private BigDecimal longitude;
 
-    public Coordinate(
-            BigDecimal latitude,
-            BigDecimal longitude
-    ) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
+	public Coordinate(
+		BigDecimal latitude,
+		BigDecimal longitude
+	) {
+		validateLatitude(latitude);
+		validateLongitude(longitude);
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public void validateLatitude(BigDecimal latitude) {
+		if (isNotInRange(latitude, new BigDecimal("124"), new BigDecimal("132"))) {
+			throw new IllegalArgumentException("한국 내의 좌표만 입력해주세요.");
+		}
+	}
+
+	public void validateLongitude(BigDecimal longitude) {
+		if (isNotInRange(longitude, new BigDecimal("33"), new BigDecimal("43"))) {
+			throw new IllegalArgumentException("한국 내의 좌표만 입력해주세요.");
+		}
+	}
+
+	private boolean isNotInRange(BigDecimal value, BigDecimal lowerBound, BigDecimal upperBound) {
+		return value.compareTo(lowerBound) < 0 || value.compareTo(upperBound) > 0;
+	}
 
 }
