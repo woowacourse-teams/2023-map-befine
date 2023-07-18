411edf387b39e7f25c085763f5bf9fd5e7280d2a
semnil5202 <semnil5202@gmail.com>
2023-07-18 17:18:04 +0900

    feat: 페이지 컴포넌트 Route 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import Layout from './components/common/Layout';
 import Home from './pages/Home';
 import PinDetail from './pages/PinDetail';
 import Pins from './pages/Pins';
+import NewTopic from './pages/NewTopic';
+import NewPin from './pages/NewPin';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="topics/:topicId" element={<Pins />} />
+          <Route path="new-topic" element={<NewTopic />} />
+          <Route path="new-pin" element={<NewPin topicName="" />} />
         </Routes>
       </Layout>
     </>
