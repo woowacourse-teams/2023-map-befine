eef997dfaba884f56ad9f78573d98172f67fe405
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-07-18 14:15:02 +0900

    feat: MSW 초기 설정 및 구동

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,6 +9,11 @@ const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 root.render(
   <ThemeProvider theme={theme}>
     <GlobalStyle />
