5bff1d52bb43f951b107b43d53ccab3e1214ad34
jaeyeon kim <jakind@naver.com>
2023-07-18 17:26:01 +0900

    refactor: BigDecimal 소수점 범위 변경
    Co-authored-by: cpot5620 <cpot5620@gmail.com>

--- backend/src/main/java/com/mapbefine/mapbefine/entity/Coordinate.java
+++ backend/src/main/java/com/mapbefine/mapbefine/entity/Coordinate.java
@@ -2,17 +2,22 @@ package com.mapbefine.mapbefine.entity;
 
 import static lombok.AccessLevel.PROTECTED;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.math.BigDecimal;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor(access = PROTECTED)
 @Getter
+@Builder
 public class Coordinate {
 
+    @Column(precision = 18, scale = 15)
     private BigDecimal latitude;
+    @Column(precision = 18, scale = 15)
     private BigDecimal longitude;
 
     public Coordinate(
