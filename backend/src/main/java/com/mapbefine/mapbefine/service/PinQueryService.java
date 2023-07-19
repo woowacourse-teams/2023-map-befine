dd8ff33e1f1353499dae92ae841b96b561eb0569
jaeyeon kim <jakind@naver.com>
2023-07-19 14:51:50 +0900

    feat: 핀 상세 조회 서비스 구현 [#40]
    Co-authored-by: cpot5620 <cpot5620@gmail.com>

--- backend/src/main/java/com/mapbefine/mapbefine/service/PinQueryService.java
+++ backend/src/main/java/com/mapbefine/mapbefine/service/PinQueryService.java
@@ -1,10 +1,9 @@
 package com.mapbefine.mapbefine.service;
 
+import com.mapbefine.mapbefine.dto.PinDetailResponse;
 import com.mapbefine.mapbefine.dto.PinResponse;
 import com.mapbefine.mapbefine.entity.Pin;
-import com.mapbefine.mapbefine.repository.LocationRepository;
 import com.mapbefine.mapbefine.repository.PinRepository;
-import com.mapbefine.mapbefine.repository.TopicRepository;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -20,10 +19,18 @@ public class PinQueryService {
         this.pinRepository = pinRepository;
     }
 
-    public List<PinResponse> findAll(){
+    public List<PinResponse> findAll() {
         return pinRepository.findAll()
                 .stream()
                 .map(PinResponse::from)
                 .collect(Collectors.toList());
     }
+
+    public PinDetailResponse findById(Long pinId) {
+        Pin pin = pinRepository.findById(pinId)
+                .orElseThrow(NoSuchElementException::new);
+
+        return PinDetailResponse.from(pin);
+    }
+
 }
