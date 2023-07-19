11caa0b81138838e0ffd7cf5bfd608f355f44c71
semnil5202 <semnil5202@gmail.com>
2023-07-19 21:46:23 +0900

    refactor: 핀 ID에 맞게 PinDetail 페이지를 출력하도록 수정

--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -1,25 +1,14 @@
 import { rest } from 'msw';
 import topics from './db/topics';
 import detailTopic from './db/detailTopic';
+import tempPins from './db/tempPins';
 
 export const handlers = [
   // 포스트 목록
   rest.get('/pin/:id', (req, res, ctx) => {
     const pinId = req.params.id;
 
-    return res(
-      ctx.status(200),
-      ctx.json({
-        id: pinId,
-        name: '오또상스시',
-        address: '서울특별시 강남구 역삼동 123-456',
-        description:
-          '초밥을 파는 곳입니다. 점심 특선 있고 초밥 질이 괜찮습니다. 가격대도 다른 곳에 비해서 양호한 편이고 적당히 생각날 때 가면 좋을 것 같습니다.',
-        latitude: '핀 위도',
-        longitude: '핀 경도',
-        updatedAt: '2023-07-12',
-      }),
-    );
+    return res(ctx.status(200), ctx.json(tempPins[pinId - 1]));
   }),
 
   // 토픽 목록
--- frontend/src/pages/SelectedTopic.tsx
+++ frontend/src/pages/SelectedTopic.tsx
@@ -35,6 +35,7 @@ const SelectedTopic = () => {
     }
   }, []);
 
+  if (!topicId) return <></>;
   if (!topicDetail) return <></>;
 
   return (
