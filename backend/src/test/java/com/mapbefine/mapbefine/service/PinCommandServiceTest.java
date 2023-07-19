e1388912993b492d2c5ead558de7bbc7219b85de
cpot5620 <cpot5620@gmail.com>
2023-07-20 00:29:13 +0900

    test: PinService 핀 수정 및 삭제 테스트 구현
    Co-authored-by: kpeel5839 <jakind@naver.com>

--- backend/src/test/java/com/mapbefine/mapbefine/service/PinCommandServiceTest.java
+++ backend/src/test/java/com/mapbefine/mapbefine/service/PinCommandServiceTest.java
@@ -2,10 +2,13 @@ package com.mapbefine.mapbefine.service;
 
 import com.mapbefine.mapbefine.dto.PinCreateRequest;
 import com.mapbefine.mapbefine.dto.PinDetailResponse;
+import com.mapbefine.mapbefine.dto.PinUpdateRequest;
 import com.mapbefine.mapbefine.entity.Coordinate;
 import com.mapbefine.mapbefine.entity.Location;
+import com.mapbefine.mapbefine.entity.Pin;
 import com.mapbefine.mapbefine.entity.Topic;
 import com.mapbefine.mapbefine.repository.LocationRepository;
+import com.mapbefine.mapbefine.repository.PinRepository;
 import com.mapbefine.mapbefine.repository.TopicRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,15 +21,20 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Transactional
 @SpringBootTest
 class PinCommandServiceTest {
+
     @Autowired
     private PinCommandService pinCommandService;
 
     @Autowired
     private PinQueryService pinQueryService;
+
+    @Autowired
+    private PinRepository pinRepository;
 
     @Autowired
     private LocationRepository locationRepository;
@@ -48,13 +56,7 @@ class PinCommandServiceTest {
         BigDecimal latitude = BigDecimal.valueOf(37.123456);
         BigDecimal longitude = BigDecimal.valueOf(127.123456);
         Coordinate coordinate = new Coordinate(latitude, longitude);
-        Location location = new Location(
-                "parcel",
-                "road",
-                coordinate,
-                "legalDongCode"
-        );
-        locationRepository.save(location);
+        Location location = saveLocation(coordinate);
 
         // when
         PinCreateRequest request = new PinCreateRequest(
@@ -87,13 +89,24 @@ class PinCommandServiceTest {
                 .isEqualTo(expected);
     }
 
+    private Location saveLocation(Coordinate coordinate) {
+        Location location = new Location(
+                "parcel",
+                "road",
+                coordinate,
+                "legalDongCode"
+        );
+
+        return locationRepository.save(location);
+    }
+
     @Test
     @DisplayName("핀을 저장하려는 존재하지 않는 위치(Location)가 존재하지 않으면 위치를 저장하고 핀을 저장한다.")
     public void saveIfNotExistLocation_Success() {
         // given
         BigDecimal latitude = BigDecimal.valueOf(37.123456);
         BigDecimal longitude = BigDecimal.valueOf(127.123456);
-        locationRepository.deleteAll();
+
         // when
         PinCreateRequest request = new PinCreateRequest(
                 topic.getId(),
@@ -127,5 +140,140 @@ class PinCommandServiceTest {
         assertThat(actual).usingRecursiveComparison()
                 .ignoringFields("updatedAt")
                 .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("제약 사항(name, description)을 지킨 정보로 핀의 정보를 수정하면 핀이 수정된다.")
+    public void update_Success() {
+        // given
+        BigDecimal latitude = BigDecimal.valueOf(37.123456);
+        BigDecimal longitude = BigDecimal.valueOf(127.123456);
+        Coordinate coordinate = new Coordinate(latitude, longitude);
+        saveLocation(coordinate);
+
+        PinCreateRequest createRequest = new PinCreateRequest(
+                topic.getId(),
+                "name",
+                "description",
+                "address",
+                "legalDongCode",
+                latitude,
+                longitude
+        );
+        Long savedPinId = pinCommandService.save(createRequest);
+
+        // when
+        PinUpdateRequest updateRequest = new PinUpdateRequest("updatedName", "updatedDescription");
+        pinCommandService.update(savedPinId, updateRequest);
+
+        // then
+        PinDetailResponse actual = pinQueryService.findById(savedPinId);
+        PinDetailResponse expected = new PinDetailResponse(
+                savedPinId,
+                "updatedName",
+                "address",
+                "updatedDescription",
+                latitude.toString(),
+                longitude.toString(),
+                null
+        );
+
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringFields("updatedAt")
+                .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("제약 사항(name, description)을 지키지 않은 정보로 핀의 정보를 수정하면 핀이 수정되지 않는다.")
+    public void update_Fail() {
+        // given
+        BigDecimal latitude = BigDecimal.valueOf(37.123456);
+        BigDecimal longitude = BigDecimal.valueOf(127.123456);
+        Coordinate coordinate = new Coordinate(latitude, longitude);
+        saveLocation(coordinate);
+
+        PinCreateRequest createRequest = new PinCreateRequest(
+                topic.getId(),
+                "name",
+                "description",
+                "address",
+                "legalDongCode",
+                latitude,
+                longitude
+        );
+        Long savedPinId = pinCommandService.save(createRequest);
+
+        // when then
+        PinUpdateRequest updateRequest = new PinUpdateRequest("", "updatedDescription");
+        assertThatThrownBy(() -> pinCommandService.update(savedPinId, updateRequest))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("핀을 삭제하면 soft-deleting 된다.")
+    public void removeById_Success() {
+        // given
+        BigDecimal latitude = BigDecimal.valueOf(37.123456);
+        BigDecimal longitude = BigDecimal.valueOf(127.123456);
+        Coordinate coordinate = new Coordinate(latitude, longitude);
+        saveLocation(coordinate);
+
+        PinCreateRequest createRequest = new PinCreateRequest(
+                topic.getId(),
+                "name",
+                "description",
+                "address",
+                "legalDongCode",
+                latitude,
+                longitude
+        );
+        Long savedPinId = pinCommandService.save(createRequest);
+
+        // when
+        Pin pin = pinRepository.findById(savedPinId).get();
+        assertThat(pin.isDeleted()).isFalse();
+
+        pinCommandService.removeById(savedPinId);
+
+        // then
+        Pin deletedPin = pinRepository.findById(savedPinId).get();
+
+        assertThat(deletedPin.isDeleted()).isTrue();
+    }
+
+    @Test
+    @DisplayName("토픽을 삭제하면 이와 관련된 모든 핀들이 soft-deleting 된다.")
+    public void removeAllByTopicId_Success() {
+        // given
+        BigDecimal latitude = BigDecimal.valueOf(37.123456);
+        BigDecimal longitude = BigDecimal.valueOf(127.123456);
+        Coordinate coordinate = new Coordinate(latitude, longitude);
+        saveLocation(coordinate);
+
+        PinCreateRequest createRequest = new PinCreateRequest(
+                topic.getId(),
+                "name",
+                "description",
+                "address",
+                "legalDongCode",
+                latitude,
+                longitude
+        );
+
+        for (int i = 0; i < 10; i++) {
+            pinCommandService.save(createRequest);
+        }
+
+        // when
+        Topic findTopicBeforeDeleting = topicRepository.findById(topic.getId()).get();
+        assertThat(findTopicBeforeDeleting.getPins()).extractingResultOf("isDeleted")
+                .doesNotContain(true);
+
+        pinCommandService.removeAllByTopicId(topic.getId());
+        // then
+        Topic findTopicAfterDeleting = topicRepository.findById(topic.getId()).get();
+
+        assertThat(findTopicAfterDeleting.getPins()).extractingResultOf("isDeleted")
+                .doesNotContain(false);
     }
 }
