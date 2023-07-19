a313400cc5886c6d5be36bc26e4708d7db1c5c42
jaeyeon kim <jakind@naver.com>
2023-07-19 15:34:37 +0900

    fix: 핀 삭제 로직 오류 수정 [#43]
    Co-authored-by: cpot5620 <cpot5620@gmail.com>

--- backend/src/main/java/com/mapbefine/mapbefine/repository/PinRepository.java
+++ backend/src/main/java/com/mapbefine/mapbefine/repository/PinRepository.java
@@ -2,13 +2,16 @@ package com.mapbefine.mapbefine.repository;
 
 import com.mapbefine.mapbefine.entity.Pin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PinRepository extends JpaRepository<Pin, Long> {
+    @Modifying
     @Query("update Pin p set p.isDeleted = true where p.topic.id = :topidId")
     void deleteAllByTopicId(@Param("topicId") Long topicId);
 
+    @Modifying
     @Query("update Pin p set p.isDeleted = true where p.id = :pinId")
     void deleteById(@Param("pinId") Long pinId);
 }
