12db24c1a57655a82e92eda6887e1f308890663b
cpot5620 <cpot5620@gmail.com>
2023-07-20 21:51:01 +0900

    test: Coordinate 테스트 중첩 클래스 제거(#84)

--- backend/src/test/java/com/mapbefine/mapbefine/entity/CoordinateTest.java
+++ backend/src/test/java/com/mapbefine/mapbefine/entity/CoordinateTest.java
@@ -1,66 +1,57 @@
 package com.mapbefine.mapbefine.entity;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
-import java.math.BigDecimal;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class CoordinateTest {
 
-    @Nested
-    @DisplayName("위도의 값이 33~43사이의 값")
-    class validateLatitude {
-        @ParameterizedTest
-        @ValueSource(strings = {"33.1", "42.9"})
-        @DisplayName("이면 통과한다.")
-        void validateLatitude_Success(String input) {
-            BigDecimal latitude = new BigDecimal(input);
+    @ParameterizedTest
+    @ValueSource(strings = {"32.9", "43.1"})
+    @DisplayName("위도의 값이 33~43사이의 값이 아니면 실패한다.")
+    void validateLatitude_Fail(String input) {
+        BigDecimal latitude = new BigDecimal(input);
 
-            assertDoesNotThrow(() -> new Coordinate(latitude, BigDecimal.valueOf(127)));
-        }
-
-        @ParameterizedTest
-        @ValueSource(strings = {"32.9", "43.1"})
-        @DisplayName("이 아니면 실패한다.")
-        void validateLatitude_Fail(String input) {
-            BigDecimal latitude = new BigDecimal(input);
-
-            Assertions.assertThatThrownBy(() -> new Coordinate(latitude, BigDecimal.valueOf(127)))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage("한국 내의 좌표만 입력해주세요.");
-        }
+        assertThatThrownBy(() -> new Coordinate(latitude, BigDecimal.valueOf(127)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("한국 내의 좌표만 입력해주세요.");
     }
 
-    @Nested
-    @DisplayName("경도의 값이 124~132사이의 값")
-    class validateLongitude {
-        @ParameterizedTest
-        @ValueSource(strings = {"124.1", "131.9"})
-        @DisplayName("이면 통과한다.")
-        void validateLongitude_Success(String input) {
-            BigDecimal longitude = new BigDecimal(input);
+    @ParameterizedTest
+    @ValueSource(strings = {"33.1", "42.9"})
+    @DisplayName("위도의 값이 33~43사이의 값이면 통과한다.")
+    void validateLatitude_Success(String input) {
+        BigDecimal latitude = new BigDecimal(input);
 
-            assertDoesNotThrow(() -> new Coordinate(BigDecimal.valueOf(37), longitude));
-        }
-
-        @ParameterizedTest
-        @ValueSource(strings = {"123.9", "132.1"})
-        @DisplayName("이 아니면 실패한다.")
-        void validateLongitude_Fail(String input) {
-            BigDecimal longitude = new BigDecimal(input);
-
-            Assertions.assertThatThrownBy(() -> new Coordinate(BigDecimal.valueOf(37), longitude))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage("한국 내의 좌표만 입력해주세요.");
-        }
+        assertDoesNotThrow(() -> new Coordinate(latitude, BigDecimal.valueOf(127)));
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"124.1", "131.9"})
+    @DisplayName("경도의 값이 124~132사이의 값이면 통과한다.")
+    void validateLongitude_Success(String input) {
+        BigDecimal longitude = new BigDecimal(input);
+
+        assertDoesNotThrow(() -> new Coordinate(BigDecimal.valueOf(37), longitude));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123.9", "132.1"})
+    @DisplayName("경도의 값이 124~132사이의 값이 아니면 실패한다.")
+    void validateLongitude_Fail(String input) {
+        BigDecimal longitude = new BigDecimal(input);
+
+        assertThatThrownBy(() -> new Coordinate(BigDecimal.valueOf(37), longitude))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("한국 내의 좌표만 입력해주세요.");
+    }
 
     @Test
     @DisplayName("좌표 사이의 거리를 계산한다.")
@@ -78,7 +69,7 @@ class CoordinateTest {
 
     @Test
     @DisplayName("좌표 사이의 거리를 계산한다.")
-    void calculateDistance3() {
+    void calculateDistance2() {
         // given
         Coordinate coordinate = new Coordinate(BigDecimal.valueOf(37.5909374), BigDecimal.valueOf(127.1537482));
         Coordinate otherCoordinate = new Coordinate(BigDecimal.valueOf(37.610454), BigDecimal.valueOf(127.2050749));
@@ -92,7 +83,7 @@ class CoordinateTest {
 
     @Test
     @DisplayName("좌표 사이의 거리를 계산한다.")
-    void calculateDistance2() {
+    void calculateDistance3() {
         // given
         Coordinate coordinate = new Coordinate(BigDecimal.valueOf(37.6273677), BigDecimal.valueOf(127.0447364));
         Coordinate otherCoordinate = new Coordinate(BigDecimal.valueOf(37.6273777), BigDecimal.valueOf(127.0447364));
