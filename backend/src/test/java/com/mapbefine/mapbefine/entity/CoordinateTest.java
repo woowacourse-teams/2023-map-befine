64a34405610a2914be090456125409e902549207
cpot5620 <cpot5620@gmail.com>
2023-07-25 20:25:39 +0900

    test: 좌표 거리 계산 테스트 ParameterizedTest 적용

--- backend/src/test/java/com/mapbefine/mapbefine/entity/CoordinateTest.java
+++ backend/src/test/java/com/mapbefine/mapbefine/entity/CoordinateTest.java
@@ -1,11 +1,13 @@
 package com.mapbefine.mapbefine.entity;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -53,46 +55,37 @@ class CoordinateTest {
                 .hasMessage("한국 내의 좌표만 입력해주세요.");
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource(value = "calculateDistanceProvider")
     @DisplayName("좌표 사이의 거리를 계산한다.")
-    void calculateDistance1() {
-        // given
-        Coordinate coordinate = new Coordinate(BigDecimal.valueOf(37.6273677), BigDecimal.valueOf(127.0447364));
-        Coordinate otherCoordinate = new Coordinate(BigDecimal.valueOf(37.6273438), BigDecimal.valueOf(127.0447853));
-
+    void calculateDistance(Coordinate coordinate, Coordinate other, BigDecimal underBound, BigDecimal upperBound) {
         // when
-        BigDecimal result = coordinate.calculateDistance(otherCoordinate);
+        BigDecimal result = coordinate.calculateDistance(other);
 
         // then
-        assertThat(result).isBetween(BigDecimal.valueOf(500 * 0.98), BigDecimal.valueOf(500 * 1.02));
+        assertThat(result).isBetween(underBound, upperBound);
     }
 
-    @Test
-    @DisplayName("좌표 사이의 거리를 계산한다.")
-    void calculateDistance2() {
-        // given
-        Coordinate coordinate = new Coordinate(BigDecimal.valueOf(37.5909374), BigDecimal.valueOf(127.1537482));
-        Coordinate otherCoordinate = new Coordinate(BigDecimal.valueOf(37.610454), BigDecimal.valueOf(127.2050749));
-
-        // when
-        BigDecimal result = coordinate.calculateDistance(otherCoordinate);
-
-        // then
-        assertThat(result).isBetween(BigDecimal.valueOf(500000 * 0.98), BigDecimal.valueOf(500000 * 1.02));
-    }
-
-    @Test
-    @DisplayName("좌표 사이의 거리를 계산한다.")
-    void calculateDistance3() {
-        // given
-        Coordinate coordinate = new Coordinate(BigDecimal.valueOf(37.6273677), BigDecimal.valueOf(127.0447364));
-        Coordinate otherCoordinate = new Coordinate(BigDecimal.valueOf(37.6273777), BigDecimal.valueOf(127.0447364));
-
-        // when
-        BigDecimal result = coordinate.calculateDistance(otherCoordinate);
-
-        // then
-        assertThat(result).isBetween(BigDecimal.valueOf(111 * 0.98), BigDecimal.valueOf(111 * 1.02));
+    static Stream<Arguments> calculateDistanceProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new Coordinate(BigDecimal.valueOf(37.6273677), BigDecimal.valueOf(127.0447364)),
+                        new Coordinate(BigDecimal.valueOf(37.6273438), BigDecimal.valueOf(127.0447853)),
+                        BigDecimal.valueOf(500 * 0.98),
+                        BigDecimal.valueOf(500 * 1.02)
+                ),
+                Arguments.of(
+                        new Coordinate(BigDecimal.valueOf(37.5909374), BigDecimal.valueOf(127.1537482)),
+                        new Coordinate(BigDecimal.valueOf(37.610454), BigDecimal.valueOf(127.2050749)),
+                        BigDecimal.valueOf(500000 * 0.98),
+                        BigDecimal.valueOf(500000 * 1.02)
+                ),
+                Arguments.of(
+                        new Coordinate(BigDecimal.valueOf(37.6273677), BigDecimal.valueOf(127.0447364)),
+                        new Coordinate(BigDecimal.valueOf(37.6273777), BigDecimal.valueOf(127.0447364)),
+                        BigDecimal.valueOf(111 * 0.98),
+                        BigDecimal.valueOf(111 * 1.02))
+        );
     }
 
 }
