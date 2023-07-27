7d8ae060d599486074ae145f454dbade5e838be8
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-07-27 17:35:34 +0900

    feat: 선택한 토픽 기준으로 지도 표시 범위 이동

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useContext, useEffect } from 'react';
+import { forwardRef, useContext, useEffect, useRef } from 'react';
 import Flex from './common/Flex';
 import { CoordinatesContext } from '../context/CoordinatesContext';
 import { MarkerContext } from '../context/MarkerContext';
@@ -7,6 +7,7 @@ const Map = (props: any, ref: any) => {
   const { map } = props;
   const { coordinates } = useContext(CoordinatesContext);
   const { markers, createMarkers, removeMarkers } = useContext(MarkerContext);
+  const bounds = useRef(new window.Tmapv2.LatLngBounds());
 
   useEffect(() => {
     // 마커들을 모두 지도에서 제거
@@ -17,8 +18,21 @@ const Map = (props: any, ref: any) => {
     // 새로운 마커 추가
     if (coordinates.length > 0) {
       createMarkers(map);
+
+      // 마커들을 모두 포함하는 경계를 계산
     }
   }, [coordinates, map]);
+
+  useEffect(() => {
+    // 마커들을 모두 포함하는 경계를 계산
+    if (markers.length > 0) {
+      bounds.current = new window.Tmapv2.LatLngBounds();
+      markers.forEach((marker: any) => {
+        bounds.current.extend(marker.getPosition());
+      });
+      map.fitBounds(bounds.current);
+    }
+  }, [markers, map]);
 
   return <Flex flex="1" id="map" ref={ref} height="100vh" width="100%" />;
 };
