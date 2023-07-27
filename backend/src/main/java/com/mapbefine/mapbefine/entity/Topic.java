7c6bf62abde0dbd3274d561a5277acb2685d06f3
yoondgu <doyou2ing@naver.com>
2023-07-27 09:58:14 +0900

    refactor: 토픽과 핀의 이름, 길이 예외 메시지 수정

--- backend/src/main/java/com/mapbefine/mapbefine/entity/Topic.java
+++ backend/src/main/java/com/mapbefine/mapbefine/entity/Topic.java
@@ -58,19 +58,19 @@ public class Topic extends BaseEntity {
 
     private void validateName(String name) {
         if (name == null) {
-            throw new IllegalArgumentException("name null");
+            throw new IllegalArgumentException("토픽 이름은 필수입니다.");
         }
         if (name.isBlank() || name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("이름 길이 이상");
+            throw new IllegalArgumentException("토픽 이름의 길이는 1자 이상 " + MAX_NAME_LENGTH + "자 이하여야 합니다.");
         }
     }
 
     private void validateDescription(String description) {
         if (description == null) {
-            throw new IllegalArgumentException("description null");
+            throw new IllegalArgumentException("토픽 설명은 필수입니다.");
         }
         if (description.isBlank() || description.length() > MAX_DESCRIPTION_LENGTH) {
-            throw new IllegalArgumentException("description 길이 이상");
+            throw new IllegalArgumentException("토픽 설명의 길이는 1자 이상 " + MAX_NAME_LENGTH + "자 이하여야 합니다.");
         }
     }
 
