0dec766e656c579818e20146773f45508a70d01d
junpakPark <junpak.park@gmail.com>
2023-07-31 22:59:36 +0900

    test: Topic 패키지 테스트 추가

---
+++ backend/src/test/java/com/mapbefine/mapbefine/common/entity/ImageTest.java
@@ -0,0 +1,44 @@
+package com.mapbefine.mapbefine.common.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class ImageTest {
+
+    @Nested
+    class validate {
+
+        @Test
+        @DisplayName("잘못된 형식의 URL을 입력하면 예외가 발생한다.")
+        void fail() {
+            //given
+            String validImageUrl = "https://example.com/image.jpg";
+
+            //when
+            Image image = Image.of(validImageUrl);
+
+            //then
+            assertThat(image).isNotNull();
+            assertThat(image.getImageUrl()).isEqualTo(validImageUrl);
+        }
+
+        @Test
+        @DisplayName("정확한 URL을 입력하면 객체가 정상적으로 생성된다.")
+        void success() {
+            //given
+            String invalidImageUrl = "https://example.com/notimage";
+
+            //when
+            //then
+            assertThatThrownBy(() -> Image.of(invalidImageUrl))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("잘못된 형식의 URL입니다.");
+        }
+
+    }
+
+}
---
+++ backend/src/test/java/com/mapbefine/mapbefine/topic/domain/TopicTest.java
@@ -0,0 +1,92 @@
+package com.mapbefine.mapbefine.topic.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mapbefine.mapbefine.location.LocationFixture;
+import com.mapbefine.mapbefine.member.MemberFixture;
+import com.mapbefine.mapbefine.member.domain.Role;
+import com.mapbefine.mapbefine.pin.Domain.Pin;
+import com.mapbefine.mapbefine.pin.PinFixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TopicTest {
+
+    private Topic topic;
+    private Pin pin;
+
+    @BeforeEach
+    void setUp() {
+        topic = Topic.of(
+                "매튜의 산스장",
+                "매튜가 엄마 몰래 찾는 산스장",
+                "https://example.com/image.jpg",
+                Publicity.PUBLIC,
+                Permission.GROUP_ONLY,
+                MemberFixture.create(Role.USER)
+        );
+
+        pin = PinFixture.create(LocationFixture.create(), topic);
+    }
+
+    @Test
+    void updateTopicInfo() {
+        //given
+        String name = "New Topic";
+        String description = "New Description";
+        String imageUrl = "https://example.com/image.png";
+
+        //when
+        topic.updateTopicInfo(
+                name,
+                description,
+                imageUrl
+        );
+        TopicInfo topicInfo = topic.getTopicInfo();
+
+        //then
+        assertThat(topicInfo.getName()).isEqualTo(name);
+        assertThat(topicInfo.getDescription()).isEqualTo(description);
+        assertThat(topicInfo.getImageUrl()).isEqualTo(imageUrl);
+    }
+
+    @Test
+    void updateTopicStatus() {
+        //given
+        Publicity publicity = Publicity.PRIVATE;
+        Permission permission = Permission.GROUP_ONLY;
+
+        //when
+        topic.updateTopicStatus(
+                publicity,
+                permission
+        );
+        TopicStatus topicStatus = topic.getTopicStatus();
+
+        //then
+        assertThat(topicStatus.getPublicity()).isEqualTo(publicity);
+        assertThat(topicStatus.getPermission()).isEqualTo(permission);
+    }
+
+    @Test
+    void countPins() {
+        //given
+        topic.addPin(pin);
+
+        //when
+        int pinCounts = topic.countPins();
+
+        //then
+        assertThat(pinCounts).isEqualTo(2);
+        // 핀이 처음 생성됐을 때, 연관관계 매핑으로 인해 하나 추가된 후 해당 메서드에서 하나 더 추가됌
+    }
+
+    @Test
+    void addPin() {
+        //when
+        topic.addPin(pin);
+
+        //then
+        assertThat(topic.getPins()).contains(pin);
+    }
+}
