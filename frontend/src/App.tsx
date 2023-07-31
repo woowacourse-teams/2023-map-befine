d84e34f7a974ef4e96473f55af2d6ccd4dce8bc1
Patrick <kuyt3122@naver.com>
2023-07-31 13:48:40 +0900

    feat: context api를 이용해 전역변수 사용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import TagContextProvider from './store/TagId';
+import TopicsIdContextProvider from './store/TopicsId';
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <TagContextProvider>
+      <TopicsIdContextProvider>
+        <RouterProvider router={router} />;
+      </TopicsIdContextProvider>
+    </TagContextProvider>
+  );
 };
 
 export default App;
