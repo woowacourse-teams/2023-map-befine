5a7b59c1c78ea794e0ab2275632d78005fb33aec
junpakPark <junpak.park@gmail.com>
2023-08-01 22:41:54 +0900

    refactor: LocationRepository 불필요 메서드 정리 및 테스트 코드 리팩터링

--- backend/src/test/java/com/mapbefine/mapbefine/location/domain/LocationRepositoryTest.java
+++ backend/src/test/java/com/mapbefine/mapbefine/location/domain/LocationRepositoryTest.java
@@ -1,9 +1,10 @@
 package com.mapbefine.mapbefine.location.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import com.mapbefine.mapbefine.location.AddressFixture;
 import java.util.List;
 import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -17,80 +18,56 @@ class LocationRepositoryTest {
     @Autowired
     private LocationRepository locationRepository;
 
-    @ParameterizedTest
-    @MethodSource(value = "coordinates_Success")
-    @DisplayName("범위 내의 Pin을 검색한다.")
-    void findAllByRectangle_Success(double latitude, double longitude) {
-        // given
-        double distance = 0.0001;
-        Coordinate coordinate = Coordinate.of(latitude, longitude);
-        Address address = new Address(
-                "parcel",
-                "road",
-                "legalDongCode"
-        );
-        Location location = new Location(
-                address,
-                coordinate
-        );
-        locationRepository.save(location);
+    private Coordinate baseCoordinate;
+    private Location meter;
+    private Location tenMeter;
+    private Location threeKiloMeter;
+    private Location tenKiloMeter;
 
 
-        // when
-        List<Location> locations = locationRepository.findAllByRectangle(
-                35,
-                127,
-                distance
-        );
-
-        // then
-        assertThat(locations).hasSize(1);
+    @BeforeEach
+    void setUp() {
+        baseCoordinate = Coordinate.of(37.6273677, 127.0447364);
+        meter = new Location(
+                AddressFixture.create(),
+                Coordinate.of(37.6273644, 127.0447427)
+        ); // 직선 거리 약 0.669 미터
+        tenMeter = new Location(
+                AddressFixture.create(),
+                Coordinate.of(37.6274194, 127.0447922)
+        ); // 직선 거리 약 7.565 미터
+        threeKiloMeter = new Location(
+                AddressFixture.create(),
+                Coordinate.of(37.6316286, 127.0650012)
+        ); // 직선 거리 약 1846.414 미터
+        tenKiloMeter = new Location(
+                AddressFixture.create(),
+                Coordinate.of(37.6507643, 127.1115283)
+        ); // 직선 거리 약 6430.905 미터
     }
 
-    static Stream<Arguments> coordinates_Success() {
-        return Stream.of(
-                Arguments.of(34.9999, 126.9999),
-                Arguments.of(35.0001, 126.9999),
-                Arguments.of(34.9999, 127.0001),
-                Arguments.of(35.0001, 127.0001)
-        );
-    }
 
     @ParameterizedTest
     @MethodSource(value = "coordinates_Fail")
-    @DisplayName("범위 내의 Pin을 검색한다.")
-    void findAllByRectangle_Fail(double latitude, double longitude) {
-        // given
-        double distance = 0.0001;
-        Coordinate coordinate = Coordinate.of(latitude, longitude);
-        Address address = new Address(
-                "지번주소",
-                "도로명주소",
-                "111000"
-        );
-        Location location = new Location(
-                address,
-                coordinate
-        );
-        locationRepository.save(location);
+    @DisplayName("범위 내의 Pin을 검색한다")
+    void findAllByCoordinateAndDistanceInMeters(double distance, int size) {
+        //given
+        locationRepository.saveAll(List.of(meter, tenMeter, threeKiloMeter, tenKiloMeter));
 
-        // when
-        List<Location> locations = locationRepository.findAllByRectangle(
-                35,
-                127,
-                distance
-        );
+        //when
+        List<Location> locations = locationRepository.findAllByCoordinateAndDistanceInMeters(
+                baseCoordinate, distance);
 
-        // then
-        assertThat(locations).isEmpty();
+        //then
+        Assertions.assertThat(locations).hasSize(size);
     }
 
     static Stream<Arguments> coordinates_Fail() {
         return Stream.of(
-                Arguments.of(34.9998, 126.9998),
-                Arguments.of(35.0002, 126.9998),
-                Arguments.of(34.9998, 127.0002),
-                Arguments.of(35.0002, 127.0002)
+                Arguments.of(1, 1),
+                Arguments.of(10, 2),
+                Arguments.of(3_000, 3),
+                Arguments.of(10_000, 4)
         );
     }
 
