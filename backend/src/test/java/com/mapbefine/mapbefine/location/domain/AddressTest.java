99970c3fbdeb3863c7b1a738e296ab78c8201655
junpakPark <junpak.park@gmail.com>
2023-08-01 22:38:06 +0900

    test: Address 테스트 추가

--- backend/src/test/java/com/mapbefine/mapbefine/location/domain/AddressTest.java
+++ backend/src/test/java/com/mapbefine/mapbefine/location/domain/AddressTest.java
@@ -1,28 +1,13 @@
 package com.mapbefine.mapbefine.location.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.mapbefine.mapbefine.location.AddressFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class AddressTest {
-
-    private static final String 지번_주소 = "지번 주소";
-    private static final String 도로명_주소 = "도로명 주소";
-    private static final String 법정동_코드 = "법정동 코드";
-
-    private Address address;
-
-    @BeforeEach
-    void setUp() {
-        address = new Address(
-                지번_주소,
-                도로명_주소,
-                법정동_코드
-        );
-    }
 
     @Nested
     @DisplayName("isSameAddress에")
@@ -31,16 +16,32 @@ class AddressTest {
         @Test
         @DisplayName("지번 주소나 도로명 주소가 동일한 true를 반환한다.")
         void success() {
-            assertThat(address.isSameAddress(지번_주소)).isTrue();
-            assertThat(address.isSameAddress(도로명_주소)).isTrue();
+            //given
+            Address address = AddressFixture.create();
+
+            //when
+            boolean 지번_주소 = address.isSameAddress(AddressFixture.지번_주소);
+            boolean 도로명_주소 = address.isSameAddress(AddressFixture.도로명_주소);
+
+            //then
+            assertThat(지번_주소).isTrue();
+            assertThat(도로명_주소).isTrue();
         }
 
         @Test
         @DisplayName("지번 주소나 도로명 주소가 동일한 true를 반환한다.")
         void fail() {
-            assertThat(address.isSameAddress("다른 지번 주소")).isFalse();
-            assertThat(address.isSameAddress("다른 도로명 주소")).isFalse();
+            //given
+            Address address = AddressFixture.create();
+
+            //when
+            boolean 다른_지번_주소 = address.isSameAddress("다른 지번 주소");
+            boolean 다른_도로명_주소 = address.isSameAddress("다른 도로명 주소");
+            //then
+            assertThat(다른_지번_주소).isFalse();
+            assertThat(다른_도로명_주소).isFalse();
         }
 
     }
+
 }
