34dd1bf998c074bd6b280c020fd4799b47ff885f
semnil5202 <semnil5202@gmail.com>
2023-08-02 20:54:31 +0900

    refactor: 토픽 추가하기 버튼 포지션 조정

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -82,7 +82,7 @@ const Home = () => {
             );
           })}
       </ul>
-      <Flex position="fixed" bottom="40px" left="130px">
+      <Flex position="fixed" bottom="40px" left="142px">
         <Button variant="primary" onClick={goToNewTopic}>
           {tagTopics.length > 0 ? '토픽 병합하기' : '토픽 추가하기'}
         </Button>
