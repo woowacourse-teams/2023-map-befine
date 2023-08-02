acef2f24961c5c17fde88ff9024012d273f00b0f
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-08-02 21:03:26 +0900

    refactor: 불필요한 의존성 제거

--- frontend/src/hooks/useMapClick.ts
+++ frontend/src/hooks/useMapClick.ts
@@ -33,5 +33,5 @@ export default function useMapClick({
         map.removeListener('click', clickHandler);
       }
     };
-  }, [getAddressFromServer, map, setClickedCoordinate]);
+  }, [map]);
 }
