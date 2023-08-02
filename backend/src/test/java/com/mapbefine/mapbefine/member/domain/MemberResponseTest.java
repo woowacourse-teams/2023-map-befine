0dad751116e9ffff162f7736163db0fecbfdd4c4
jaeyeon kim <jakind@naver.com>
2023-08-02 22:46:16 +0900

    test : memberResponse 정적 팩토리 메서드 테스트 작성

---
+++ backend/src/test/java/com/mapbefine/mapbefine/member/domain/MemberResponseTest.java
@@ -0,0 +1,32 @@
+package com.mapbefine.mapbefine.member.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mapbefine.mapbefine.member.MemberFixture;
+import com.mapbefine.mapbefine.member.dto.response.MemberResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MemberResponseTest {
+
+    @Test
+    @DisplayName("Member 를 이용하여 MemberResponse 가 정상적으로 생성된다.")
+    void createMemberResponse_success() {
+        // given
+        Member member = MemberFixture.create(
+                "member",
+                "member@naver.com",
+                Role.ADMIN
+        );
+
+        // when
+        MemberResponse memberResponse = MemberResponse.from(member);
+
+        // then
+        assertThat(memberResponse.name())
+                .isEqualTo(member.getMemberInfo().getName());
+        assertThat(memberResponse.email())
+                .isEqualTo(member.getMemberInfo().getEmail());
+    }
+
+}
