db2a5077e431b40243a98cb1e26a5f7812ee0c05
Doy <doyoungwork@gmail.com>
2023-08-02 11:17:24 +0900

    fix: CORS 허용 Origin에 개발서버, 운영서버 도메인 추가 (#171)

--- backend/src/main/java/com/mapbefine/mapbefine/config/WebMvcConfig.java
+++ backend/src/main/java/com/mapbefine/mapbefine/config/WebMvcConfig.java
@@ -11,6 +11,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://mapbefine.kro.kr")
+                .allowedOrigins("https://mapbefine.com")
                 .allowedMethods("*")
                 .exposedHeaders("Location");
     }
