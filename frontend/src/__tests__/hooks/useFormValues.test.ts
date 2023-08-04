0a181ab972694e4247a635ac775748096ae59d8c
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-08-04 09:14:34 +0900

    chore: test 2

--- frontend/src/__tests__/hooks/useFormValues.test.ts
+++ frontend/src/__tests__/hooks/useFormValues.test.ts
@@ -6,7 +6,7 @@ describe('useFormValues 테스트', () => {
   test('매개변수로 받은 초기값을 정상적으로 반환하는지 확인한다.', () => {
     const { result } = renderHook(() => useFormValues<string>('토픽 이름'));
 
-    expect(result.current.formValues).toBe('토픽 이름');
+    expect(result.current.formValues).toBe('토픽 이');
   });
 
   test('setFormValues를 통해 set할 수 있는지 확인하다.', () => {
