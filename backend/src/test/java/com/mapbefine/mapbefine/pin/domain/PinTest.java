b394b0f7a03e717efddb91bbea81e637019de7a6
yoondgu <doyou2ing@naver.com>
2023-08-06 01:48:13 +0900

    test: 테스트 클래스 내 픽스쳐 사용 방식 통일, 메서드 순서 변경

--- backend/src/test/java/com/mapbefine/mapbefine/pin/domain/PinTest.java
+++ backend/src/test/java/com/mapbefine/mapbefine/pin/domain/PinTest.java
@@ -10,33 +10,38 @@ import com.mapbefine.mapbefine.member.domain.Role;
 import com.mapbefine.mapbefine.topic.TopicFixture;
 import com.mapbefine.mapbefine.topic.domain.Topic;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PinTest {
 
-    private static final Location LOCATION = LocationFixture.create();
-    private static final Topic TOPIC = TopicFixture.createByName(
-            "topic",
-            MemberFixture.create("member", "member@naver.com", Role.ADMIN)
-    );
-    private static final Member MEMBER = Member.of(
-            "memberr",
-            "memberr@naver.com",
-            "https://map-befine-official.github.io/favicon.png",
-            Role.ADMIN
-    );
+
     private static final PinInfo VALID_PIN_INFO = PinInfo.of("name", "description");
+
+    private Location location;
+    private Topic topic;
+    private Member member;
+
+    @BeforeEach
+    void setUp() {
+        location = LocationFixture.create();
+        topic = TopicFixture.createByName(
+                "topic",
+                MemberFixture.create("member", "member@naver.com", Role.ADMIN)
+        );
+        member = MemberFixture.create("pin member", "member@naver.com", Role.ADMIN);
+    }
 
     @Test
     @DisplayName("올바른 핀 정보를 주면 성공적으로 Pin 을 생성한다.")
-    void createPinAssociatedWithLocationAndTopic_Success() {
+    void createPinAssociatedWithLocationAndTopicAndMember_validate_Success() {
         // given
         Pin pin = Pin.createPinAssociatedWithLocationAndTopicAndMember(
                 VALID_PIN_INFO,
-                LOCATION,
-                TOPIC,
-                MEMBER
+                location,
+                topic,
+                member
         );
 
         // when
@@ -48,46 +53,19 @@ class PinTest {
     }
 
     @Test
-    @DisplayName("올바른 핀 정보를 주면 성공적으로 Pin 을 수정한다.")
-    void update_Success() {
-        // given
-        Pin pin = Pin.createPinAssociatedWithLocationAndTopicAndMember(
-                PinInfo.of("before name", "before description"),
-                LOCATION,
-                TOPIC,
-                MEMBER
-        );
-        String updateName = VALID_PIN_INFO.getName();
-        String updateDescription = VALID_PIN_INFO.getDescription();
-
-        // when
-        pin.updatePinInfo(updateName, updateDescription);
-        PinInfo actual = pin.getPinInfo();
-
-        // then
-        assertThat(actual).usingRecursiveComparison()
-                .isEqualTo(PinInfo.of(updateName, updateDescription));
-    }
-
-    @Test
-    @DisplayName("Pin 을 생성하면, Location, Topic, Member 에 등록이 된다.")
-    void createPinAssociatedWithLocationAndTopicAndMember_Success() {
-        // given
-        Member mockMember = MemberFixture.create("member", "member@naver.com", Role.ADMIN);
-        Topic mockTopic = TopicFixture.createByName("topicName", mockMember);
-        Location mockLocation = LocationFixture.create();
-
-        // when
+    @DisplayName("Pin 을 생성하면, Location, Topic, Member 에 연관 관계가 등록된다.")
+    void createPinAssociatedWithLocationAndTopicAndMember_associate_Success() {
+        // given, when
         Pin pin = Pin.createPinAssociatedWithLocationAndTopicAndMember(
                 PinInfo.of("name", "description"),
-                mockLocation,
-                mockTopic,
-                mockMember
+                location,
+                topic,
+                member
         );
 
-        List<Pin> pinsInLocation = mockLocation.getPins();
-        List<Pin> pinsInTopic = mockTopic.getPins();
-        List<Pin> pinsInMember = mockMember.getCreatedPins();
+        List<Pin> pinsInLocation = location.getPins();
+        List<Pin> pinsInTopic = topic.getPins();
+        List<Pin> pinsInMember = member.getCreatedPins();
 
         // then
         assertThat(pinsInLocation).hasSize(1);
@@ -99,5 +77,27 @@ class PinTest {
                 .isEqualTo(pin);
         assertThat(pinsInMember.get(0)).usingRecursiveComparison()
                 .isEqualTo(pin);
+    }
+
+    @Test
+    @DisplayName("올바른 핀 정보를 주면 성공적으로 Pin 을 수정한다.")
+    void updatePinInfo_Success() {
+        // given
+        Pin pin = Pin.createPinAssociatedWithLocationAndTopicAndMember(
+                PinInfo.of("before name", "before description"),
+                location,
+                topic,
+                member
+        );
+        String updateName = VALID_PIN_INFO.getName();
+        String updateDescription = VALID_PIN_INFO.getDescription();
+
+        // when
+        pin.updatePinInfo(updateName, updateDescription);
+        PinInfo actual = pin.getPinInfo();
+
+        // then
+        assertThat(actual).usingRecursiveComparison()
+                .isEqualTo(PinInfo.of(updateName, updateDescription));
     }
 }
