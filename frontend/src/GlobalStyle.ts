cacafd8e6baaf81c1580ebe97517b8ceb10c111b
semnil5202 <semnil5202@gmail.com>
2023-08-07 01:18:25 +0900

    refactor: Input 컴포넌트 불필요한 속성 제거 및 GlobalStyles로 분리

--- frontend/src/GlobalStyle.ts
+++ frontend/src/GlobalStyle.ts
@@ -8,17 +8,26 @@ const GlobalStyle = createGlobalStyle`
     margin:0;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
   }
+  
   a{
     text-decoration:none;
     color: #000;
     transition: all 0.3s ease-in-out;
   }
+
   ul{
     padding-left: 0;
     list-style-type: none;
   }
+
   *{
     box-sizing:border-box;
+  }
+
+  input[type='number']::-webkit-outer-spin-button,
+  input[type='number']::-webkit-inner-spin-button {
+    appearance: none;
+    margin: 0;
   }
   `;
 
