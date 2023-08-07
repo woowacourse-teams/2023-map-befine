cfe84c01f49b23fc00f7dcf42e7f1603ce77806c
cpot5620 <cpot5620@gmail.com>
2023-08-07 20:35:06 +0900

    test: TopicRepository soft-deleting 테스트 구현

---
+++ backend/src/test/java/com/mapbefine/mapbefine/topic/domain/TopicRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.mapbefine.mapbefine.topic.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mapbefine.mapbefine.member.domain.Member;
+import com.mapbefine.mapbefine.member.domain.MemberRepository;
+import com.mapbefine.mapbefine.member.domain.Role;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class TopicRepositoryTest {
+
+    private static final Member member = new Member(
+            "쥬니",
+            "cpot5620@gmail.com",
+            "프로필사진",
+            Role.USER
+    );
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository.save(member);
+    }
+
+    @Test
+    @DisplayName("토픽을 삭제하면, soft-deleting 된다.")
+    void deleteById_Success() {
+        //given
+        Topic topic = Topic.of(
+                "토픽",
+                "토픽설명",
+                "https://example.com/image.jpg",
+                Publicity.PUBLIC,
+                Permission.ALL_MEMBERS,
+                member
+        );
+
+        topicRepository.save(topic);
+
+        assertThat(topic.isDeleted()).isFalse();
+
+        //when
+        topicRepository.deleteById(topic.getId());
+
+        //then
+        Topic deletedTopic = topicRepository.findById(topic.getId()).get();
+        assertThat(deletedTopic.isDeleted()).isTrue();
+    }
+
+}
