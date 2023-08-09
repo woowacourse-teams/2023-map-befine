a3eb4688bfbb5a82892fd1b338f1c84829dc0989
semnil5202 <semnil5202@gmail.com>
2023-08-09 20:44:41 +0900

    design: Input 및 Textarea 컴포넌트의 outline 속성 추가

--- frontend/src/components/common/Input/index.ts
+++ frontend/src/components/common/Input/index.ts
@@ -1,6 +1,10 @@
 import { styled } from 'styled-components';
 
-const Input = styled.input`
+interface InputProps {
+  hasBadWord: boolean;
+}
+
+const Input = styled.input<InputProps>`
   width: 100%;
   height: 52px;
   box-sizing: border-box;
@@ -9,6 +13,7 @@ const Input = styled.input`
   padding: ${({ theme }) => theme.spacing['2']};
   font-size: ${({ theme }) => theme.fontSize.small};
   border-radius: ${({ theme }) => theme.radius.small};
+  outline: ${({ hasBadWord }) => hasBadWord && 'solid 1px #FF4040'};
 
   &::placeholder {
     color: ${({ theme }) => theme.color.gray};
--- frontend/src/components/common/Textarea/index.ts
+++ frontend/src/components/common/Textarea/index.ts
@@ -1,6 +1,10 @@
 import { styled } from 'styled-components';
 
-const Textarea = styled.textarea`
+interface TextareaProps {
+  hasBadWord: boolean;
+}
+
+const Textarea = styled.textarea<TextareaProps>`
   width: 100%;
   height: 100px;
   box-sizing: border-box;
@@ -11,6 +15,7 @@ const Textarea = styled.textarea`
   border-radius: ${({ theme }) => theme.radius.small};
   padding: ${({ theme }) => theme.spacing['2']};
   font-size: ${({ theme }) => theme.fontSize.small};
+  outline: ${({ hasBadWord }) => hasBadWord && 'solid 1px #FF4040'};
 
   &::placeholder {
     color: ${({ theme }) => theme.color.gray};
