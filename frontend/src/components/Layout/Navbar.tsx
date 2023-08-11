5ee9e2c35db0c5b86c4dcf5798a0746d67afab5a
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-08-11 11:37:46 +0900

    feat: 툴팁 컴포넌트 구현

--- frontend/src/components/Layout/Navbar.tsx
+++ frontend/src/components/Layout/Navbar.tsx
@@ -8,6 +8,7 @@ import useNavigator from '../../hooks/useNavigator';
 import { useState } from 'react';
 import Button from '../common/Button';
 import Space from '../common/Space';
+import Tooltip from '../common/Tooltip';
 
 const Navbar = () => {
   const { routePage } = useNavigator();
@@ -26,35 +27,37 @@ const Navbar = () => {
     >
       <Home onClick={() => routePage('/')} />
       <All onClick={() => routePage('/all')} />
-      <AddButton
-        onClick={() => {
-          setShowButton((prev) => !prev);
-        }}
-      />
-      {showButton && (
-        <Flex
-          position="absolute"
-          bottom="60px"
-          $justifyContent="center"
-          padding="20px"
-          left="0"
-          width="100%"
-        >
-          <Button
-            variant="primary"
-            onClick={() => {
-              routePage('/new-topic');
-              setShowButton(false);
-            }}
+      <Tooltip content="나만의 지도를 추가해보세요" position="top">
+        <AddButton
+          onClick={() => {
+            setShowButton((prev) => !prev);
+          }}
+        />
+        {showButton && (
+          <Flex
+            position="absolute"
+            bottom="60px"
+            $justifyContent="center"
+            padding="20px"
+            left="0"
+            width="100%"
           >
-            지도 추가하기
-          </Button>
-          <Space size={4} />
-          <Button variant="primary" onClick={() => routePage('/')}>
-            핀 추가하기
-          </Button>
-        </Flex>
-      )}
+            <Button
+              variant="primary"
+              onClick={() => {
+                routePage('/new-topic');
+                setShowButton(false);
+              }}
+            >
+              지도 추가하기
+            </Button>
+            <Space size={4} />
+            <Button variant="primary" onClick={() => routePage('/')}>
+              핀 추가하기
+            </Button>
+          </Flex>
+        )}
+      </Tooltip>
       <Favorite onClick={() => routePage('/favorite')} />
       <Keep onClick={() => routePage('/keep')} />
     </Flex>
