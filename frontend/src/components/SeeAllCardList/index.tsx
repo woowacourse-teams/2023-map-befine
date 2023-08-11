57e705699d4698105fe79917657484c699852de9
semnil5202 <semnil5202@gmail.com>
2023-08-11 18:54:26 +0900

    design: 피그마 시안에 따른 전체보기 UI 변경

--- frontend/src/components/SeeAllCardList/index.tsx
+++ frontend/src/components/SeeAllCardList/index.tsx
@@ -2,7 +2,7 @@ import { Fragment, useEffect, useState } from 'react';
 import { TopicType } from '../../types/Topic';
 import { getApi } from '../../apis/getApi';
 import TopicCard from '../TopicCard';
-import Space from '../common/Space';
+import Flex from '../common/Flex';
 
 interface SeeAllCardListProps {
   url: string;
@@ -22,20 +22,20 @@ const SeeAllCardList = ({ url }: SeeAllCardListProps) => {
 
   return (
     <ul>
-      {topics &&
-        topics.map((topic, idx) => (
-          <Fragment key={topic.id}>
-            <TopicCard
-              topicShape="horizontal"
-              topicId={topic.id}
-              topicImage={topic.image}
-              topicTitle={topic.name}
-              topicPinCount={topic.pinCount}
-              topicUpdatedAt={topic.updatedAt}
-            />
-            {topics.length - 1 !== idx ? <Space size={4} /> : <></>}
-          </Fragment>
-        ))}
+      <Flex $flexWrap="wrap" $gap="20px" overflow="hidden">
+        {topics &&
+          topics.map((topic) => (
+            <Fragment key={topic.id}>
+              <TopicCard
+                topicId={topic.id}
+                topicImage={topic.image}
+                topicTitle={topic.name}
+                topicPinCount={topic.pinCount}
+                topicUpdatedAt={topic.updatedAt}
+              />
+            </Fragment>
+          ))}
+      </Flex>
     </ul>
   );
 };
--- frontend/src/pages/SeeAllTopics.tsx
+++ frontend/src/pages/SeeAllTopics.tsx
@@ -2,6 +2,8 @@ import { useLocation } from 'react-router-dom';
 import SeeAllCardList from '../components/SeeAllCardList';
 import Text from '../components/common/Text';
 import Space from '../components/common/Space';
+import { styled } from 'styled-components';
+import Box from '../components/common/Box';
 
 const SeeAllTopics = () => {
   const { state } = useLocation();
@@ -9,13 +11,21 @@ const SeeAllTopics = () => {
   const title = state.split('|')[1];
 
   return (
-    <>
-      <Text color="black" $fontSize="large" $fontWeight="bold">
+    <Wrapper>
+      <Text color="black" $fontSize="extraLarge" $fontWeight="bold">
         {title}
       </Text>
+
+      <Space size={4} />
+
       <SeeAllCardList url={url} />
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled(Box)`
+  width: 1036px;
+  margin: 0 auto;
+`;
 
 export default SeeAllTopics;
