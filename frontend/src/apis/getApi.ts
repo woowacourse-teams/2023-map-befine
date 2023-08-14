5678c42a02c858e2efc2b61f1916e5dbdf701af3
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-08-14 19:33:50 +0900

    fix: login-error

--- frontend/src/apis/getApi.ts
+++ frontend/src/apis/getApi.ts
@@ -17,8 +17,6 @@ export const getApi = async <T>(
 
   const responseData: T = await response.json();
   if (response.status !== 200) {
-    console.log(response, 'response');
-    console.log(responseData, 'responseData');
     throw new Error('API 요청에 실패했습니다.');
   }
 
