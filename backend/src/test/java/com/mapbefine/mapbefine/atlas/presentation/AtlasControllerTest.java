f8b18d3e6a72847b1586efb9046e0dedea036c61
junpakPark <junpak.park@gmail.com>
2023-08-14 16:48:56 +0900

    doc: restDocs 적용

--- backend/src/test/java/com/mapbefine/mapbefine/atlas/presentation/AtlasControllerTest.java
+++ backend/src/test/java/com/mapbefine/mapbefine/atlas/presentation/AtlasControllerTest.java
@@ -4,6 +4,7 @@ import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
+import com.mapbefine.mapbefine.atlas.application.AtlasCommandService;
 import com.mapbefine.mapbefine.atlas.application.AtlasQueryService;
 import com.mapbefine.mapbefine.common.RestDocsIntegration;
 import com.mapbefine.mapbefine.topic.dto.response.TopicResponse;
@@ -20,6 +21,8 @@ class AtlasControllerTest extends RestDocsIntegration {
 
     @MockBean
     private AtlasQueryService atlasQueryService;
+    @MockBean
+    private AtlasCommandService atlasCommandService;
 
     private final String authHeader = Base64.encodeBase64String("Basic member@naver.com".getBytes());
     private final List<TopicResponse> responses = List.of(
@@ -42,7 +45,7 @@ class AtlasControllerTest extends RestDocsIntegration {
 
     @Test
     @DisplayName("모아보기에 추가되어있는 지도 목록을 조회한다")
-    void findTopicsFromAtlas_Success() throws Exception {
+    void findTopicsFromAtlas() throws Exception {
         // when
         given(atlasQueryService.findTopicsInAtlasByMember(any()))
                 .willReturn(responses);
@@ -57,20 +60,20 @@ class AtlasControllerTest extends RestDocsIntegration {
 
     @Test
     @DisplayName("모아보기에 지도를 추가한다")
-    void addTopicToAtlas_Success() throws Exception {
+    void addTopicToAtlas() throws Exception {
         // then
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/atlas/1")
+                MockMvcRequestBuilders.post("/atlas/1")
                         .header(AUTHORIZATION, authHeader)
         ).andDo(restDocs.document());
     }
 
     @Test
     @DisplayName("모아보기에 추가되어있는 지도를 삭제한다")
-    void removeTopicFromAtlas_Success() throws Exception {
+    void removeTopicFromAtlas() throws Exception {
         // then
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/atlas/1")
+                MockMvcRequestBuilders.delete("/atlas/1")
                         .header(AUTHORIZATION, authHeader)
         ).andDo(restDocs.document());
     }
