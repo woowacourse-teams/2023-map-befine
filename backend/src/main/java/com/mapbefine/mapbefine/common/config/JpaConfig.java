1a380d00d497ed54a6d65cf40c993125c10a6489
jaeyeon kim <jakind@naver.com>
2023-08-14 14:08:07 +0900

    refactor : 테스트 실패 원인 파악

--- backend/src/main/java/com/mapbefine/mapbefine/common/config/JpaConfig.java
+++ backend/src/main/java/com/mapbefine/mapbefine/common/config/JpaConfig.java
@@ -1,11 +1,9 @@
 package com.mapbefine.mapbefine.common.config;
 
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-@ConfigurationPropertiesScan
 public class JpaConfig {
 }
