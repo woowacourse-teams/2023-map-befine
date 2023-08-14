ec98eb00e3e23c15a11e69f90faf4c3bad7fa39a
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-08-14 10:13:57 +0900

    refactor: 하단 바 메뉴 변경

--- frontend/src/components/Layout/Navbar.tsx
+++ frontend/src/components/Layout/Navbar.tsx
@@ -3,16 +3,20 @@ import Home from '../../assets/Home.svg';
 import All from '../../assets/All.svg';
 import AddButton from '../../assets/addButton.svg';
 import Favorite from '../../assets/Favorite.svg';
-import Keep from '../../assets/Keep.svg';
+import My from '../../assets/My.svg';
 import useNavigator from '../../hooks/useNavigator';
 import { useState } from 'react';
 import Button from '../common/Button';
 import Space from '../common/Space';
-import Tooltip from '../common/Tooltip';
 
 const Navbar = () => {
   const { routePage } = useNavigator();
   const [showButton, setShowButton] = useState(false);
+
+  const loginButtonClick = () => {
+    console.log('HELLO');
+    window.location.href = 'http://localhost:8080/oauth/kakao';
+  };
 
   return (
     <Flex
@@ -26,40 +30,34 @@ const Navbar = () => {
       bottom="0"
     >
       <Home onClick={() => routePage('/')} />
-      <All onClick={() => routePage('/all')} />
-      <Tooltip content="나만의 지도를 추가해보세요" position="top">
-        <AddButton
-          onClick={() => {
-            setShowButton((prev) => !prev);
-          }}
-        />
-        {showButton && (
-          <Flex
-            position="absolute"
-            bottom="60px"
-            $justifyContent="center"
-            padding="20px"
-            left="0"
-            width="100%"
+      <All onClick={() => routePage('/moabogi', '/topics|모아보기')} />
+      <AddButton onClick={() => loginButtonClick()} />
+      {showButton && (
+        <Flex
+          position="absolute"
+          bottom="60px"
+          $justifyContent="center"
+          padding="20px"
+          left="0"
+          width="100%"
+        >
+          <Button
+            variant="primary"
+            onClick={() => {
+              routePage('/new-topic');
+              setShowButton(false);
+            }}
           >
-            <Button
-              variant="primary"
-              onClick={() => {
-                routePage('/new-topic');
-                setShowButton(false);
-              }}
-            >
-              지도 추가하기
-            </Button>
-            <Space size={4} />
-            <Button variant="primary" onClick={() => routePage('/')}>
-              핀 추가하기
-            </Button>
-          </Flex>
-        )}
-      </Tooltip>
-      <Favorite onClick={() => routePage('/favorite')} />
-      <Keep onClick={() => routePage('/keep')} />
+            지도 추가하기
+          </Button>
+          <Space size={4} />
+          <Button variant="primary" onClick={() => routePage('/')}>
+            핀 추가하기
+          </Button>
+        </Flex>
+      )}
+      <Favorite onClick={() => routePage('/zzlegyeo', '/topics|즐겨찾기')} />
+      <My onClick={() => routePage('/my')} />
     </Flex>
   );
 };
