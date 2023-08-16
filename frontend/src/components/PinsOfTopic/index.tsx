c4339d208b4616bd37d604362a89b52937673605
semnil5202 <semnil5202@gmail.com>
2023-08-16 10:45:29 +0900

    refactor: 불필요한 fragment 제거

--- frontend/src/components/PinsOfTopic/index.tsx
+++ frontend/src/components/PinsOfTopic/index.tsx
@@ -19,34 +19,32 @@ const PinsOfTopic = ({
   setIsEditPinDetail,
 }: PinsOfTopicProps) => {
   return (
-    <>
-      <ul>
-        <TopicInfo
-          fullUrl={String(topicId)}
-          topicId={topicId}
-          topicImage={DEFAULT_TOPIC_IMAGE}
-          topicParticipant={1}
-          topicPinCount={topicDetail.pinCount}
-          topicTitle={topicDetail.name}
-          topicOwner={'토픽을 만든 사람'}
-          topicDescription={topicDetail.description}
-        />
-        {topicDetail.pins.map((pin, idx) => (
-          <li key={pin.id}>
-            <PinPreview
-              idx={idx}
-              pinTitle={pin.name}
-              pinLocation={pin.address}
-              pinInformation={pin.description}
-              setSelectedPinId={setSelectedPinId}
-              pinId={Number(pin.id)}
-              topicId={String(topicId)}
-              setIsEditPinDetail={setIsEditPinDetail}
-            />
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul>
+      <TopicInfo
+        fullUrl={String(topicId)}
+        topicId={topicId}
+        topicImage={DEFAULT_TOPIC_IMAGE}
+        topicParticipant={1}
+        topicPinCount={topicDetail.pinCount}
+        topicTitle={topicDetail.name}
+        topicOwner={'토픽을 만든 사람'}
+        topicDescription={topicDetail.description}
+      />
+      {topicDetail.pins.map((pin, idx) => (
+        <li key={pin.id}>
+          <PinPreview
+            idx={idx}
+            pinTitle={pin.name}
+            pinLocation={pin.address}
+            pinInformation={pin.description}
+            setSelectedPinId={setSelectedPinId}
+            pinId={Number(pin.id)}
+            topicId={String(topicId)}
+            setIsEditPinDetail={setIsEditPinDetail}
+          />
+        </li>
+      ))}
+    </ul>
   );
 };
 
