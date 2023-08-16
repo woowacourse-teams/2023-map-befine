feb84d11fca11f276d5a83dd112b3eaa61eca5db
Patrick <kuyt3122@naver.com>
2023-08-16 14:50:27 +0900

    refactor: 내 지도 목록 모달 서버 요청 후 데이터 받도록 수정

--- frontend/src/components/ModalMyTopicList/index.tsx
+++ frontend/src/components/ModalMyTopicList/index.tsx
@@ -4,137 +4,26 @@ import { getApi } from '../../apis/getApi';
 import { ModalMyTopicType } from '../../types/Topic';
 import ModalTopicCard from '../ModalTopicCard';
 
-const data = [
-  {
-    id: 1,
-    name: '준팍의 또 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 3,
-    bookmarkCount: 100,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563088131',
-  },
-  {
-    id: 2,
-    name: '준팍의 두번째 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 5,
-    bookmarkCount: 150,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563105931',
-  },
-  {
-    id: 3,
-    name: '준팍의 또 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 3,
-    bookmarkCount: 100,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563088131',
-  },
-  {
-    id: 4,
-    name: '준팍의 두번째 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 5,
-    bookmarkCount: 150,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563105931',
-  },
-  {
-    id: 5,
-    name: '준팍의 또 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 3,
-    bookmarkCount: 100,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563088131',
-  },
-  {
-    id: 6,
-    name: '준팍의 두번째 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 5,
-    bookmarkCount: 150,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563105931',
-  },
-  {
-    id: 7,
-    name: '준팍의 또 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 3,
-    bookmarkCount: 100,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563088131',
-  },
-  {
-    id: 8,
-    name: '준팍의 두번째 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 5,
-    bookmarkCount: 150,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563105931',
-  },
-  {
-    id: 9,
-    name: '준팍의 또 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 3,
-    bookmarkCount: 100,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563088131',
-  },
-  {
-    id: 10,
-    name: '준팍의 두번째 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 5,
-    bookmarkCount: 150,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563105931',
-  },
-  {
-    id: 11,
-    name: '준팍의 또 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 3,
-    bookmarkCount: 100,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563088131',
-  },
-  {
-    id: 12,
-    name: '준팍의 두번째 토픽',
-    image: 'https://map-befine-official.github.io/favicon.png',
-    pinCount: 5,
-    bookmarkCount: 150,
-    isBookmarked: true,
-    updatedAt: '2023-08-14T13:15:33.563105931',
-  },
-];
-
 const ModalMyTopicList = () => {
   const [myTopics, setMyTopics] = useState<ModalMyTopicType[]>([]);
 
   const getMyTopicFromServer = async () => {
     const serverMyTopic = await getApi<ModalMyTopicType[]>(
       'default',
-      '/bookmarks',
+      '/members/my/topics',
     );
-    setMyTopics(myTopics);
+    setMyTopics(serverMyTopic);
   };
 
-//   useEffect(() => {
-//     getMyTopicFromServer();
-//   }, []);
+    useEffect(() => {
+      getMyTopicFromServer();
+    }, []);
 
-  if (!data) return <></>;
+  if (!myTopics) return <></>;
 
   return (
     <ModalMyTopicListWrapper>
-      {data.map((topic) => (
+      {myTopics.map((topic) => (
         <Fragment key={topic.id}>
           <ModalTopicCard
             topicId={topic.id}
@@ -150,7 +39,7 @@ const ModalMyTopicList = () => {
 };
 
 const ModalMyTopicListWrapper = styled.ul`
-   display: grid;
+  display: grid;
   grid-template-columns: repeat(auto-fit, minmax(340px, 1fr));
   grid-row-gap: ${({ theme }) => theme.spacing[5]};
 `;
