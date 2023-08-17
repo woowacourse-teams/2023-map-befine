ba4817baa8ccd3528ce5588db950cdbf4155996b
junpakPark <junpak.park@gmail.com>
2023-08-17 11:43:03 +0900

    refactor: 불필요 Getter 어노테이션 제거
    Co-authored-by: yoondgu <doyou2ing@naver.com>

--- backend/src/main/java/com/mapbefine/mapbefine/common/exception/UnauthorizedException.java
+++ backend/src/main/java/com/mapbefine/mapbefine/common/exception/UnauthorizedException.java
@@ -1,9 +1,7 @@
 package com.mapbefine.mapbefine.common.exception;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
 public class UnauthorizedException extends GlobalException {
 
     public UnauthorizedException(ErrorCode errorCode) {
