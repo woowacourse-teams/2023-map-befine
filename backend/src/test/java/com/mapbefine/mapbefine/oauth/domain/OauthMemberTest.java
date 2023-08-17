7f6deaf41c97759b6755478fe54d90566e02847b
yoondgu <doyou2ing@naver.com>
2023-08-17 20:05:20 +0900

    test: 카카오 닉네임 13자 초과인 경우 테스트 수정

--- backend/src/test/java/com/mapbefine/mapbefine/oauth/domain/OauthMemberTest.java
+++ backend/src/test/java/com/mapbefine/mapbefine/oauth/domain/OauthMemberTest.java
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.Test;
 class OauthMemberTest {
 
     @Test
-    @DisplayName("소셜 로그인 닉네임의 길이가 20자 이상이면 기본 랜덤 닉네임을 부여한다.")
+    @DisplayName("소셜 로그인 닉네임의 길이가 13자 초과이면 기본 랜덤 닉네임을 부여한다.")
     void toRegisterMember() {
         OauthMember oauthMember = OauthMember.of(
-                "일이삼사오육칠팔구십일이삼사오육칠팔구십",
+                "일이삼사오육칠팔구십일이삼사",
                 "member@gmail.com",
                 "https://image.url",
                 1L,
