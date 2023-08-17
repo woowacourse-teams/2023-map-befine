86d861537bc6de3caf9716f956aab2cfd3e39520
Patrick <kuyt3122@naver.com>
2023-08-18 02:04:57 +0900

    hotfix: response 분리

--- frontend/src/apis/getApi.ts
+++ frontend/src/apis/getApi.ts
@@ -7,7 +7,6 @@ interface Headers {
   'Content-Type': string;
   [key: string]: string;
 }
-
 export const getApi = async <T>(
   type: 'tMap' | 'default' | 'login',
   url: string,
@@ -18,21 +17,17 @@ export const getApi = async <T>(
   const headers: Headers = {
     'Content-Type': 'application/json',
   };
-
   if (userToken) {
     headers['Authorization'] = `Bearer ${userToken}`;
   }
-
   const response = await fetch(apiUrl, {
     method: 'GET',
     headers: headers,
   });
-
   const responseData: T = await response.json();
   if (response.status >= 400) {
     //todo: status 상태별로 로그인 토큰 유효 검증
     throw new Error('API 요청에 실패했습니다.');
   }
-
   return responseData;
 };
