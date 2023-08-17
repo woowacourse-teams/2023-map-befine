63227b1a0a1d9cdbd404f313d96a6d0f4ddf64b7
ParkGeunCheol <72205402+GC-Park@users.noreply.github.com>
2023-08-18 01:38:44 +0900

    hotfix: 서버 요청 도메인 분리 (#354)
    * refactor: 서버 요청 도메인 분리
    
    * refactor: end of line 수정

--- frontend/src/apis/deleteApi.ts
+++ frontend/src/apis/deleteApi.ts
@@ -1,5 +1,10 @@
+const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_DEFAULT_PROD
+    : process.env.REACT_APP_API_DEFAULT_DEV;
+
 export const deleteApi = async (url: string, contentType?: string) => {
-  await fetch(`${process.env.REACT_APP_API_DEFAULT + url}`, {
+  await fetch(`${API_URL + url}`, {
     method: 'DELETE',
     headers: {
       Authorization: `Bearer ${localStorage.getItem('userToken') || ''}`,
