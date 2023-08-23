813abba8f34053ed0b1853f023dfce9df386e0b2
semnil5202 <semnil5202@gmail.com>
2023-08-24 00:43:24 +0900

    rename: 사용하지 않는 svg 제거 및 오타 수정

--- frontend/src/components/MyInfo/UpdateMyInfo.tsx
+++ frontend/src/components/MyInfo/UpdateMyInfo.tsx
@@ -1,6 +1,6 @@
 import { styled } from 'styled-components';
 import Flex from '../common/Flex';
-import InfoDefalutImg from '../../assets/InfoDefalutImg.svg';
+import ProfileDefaultImage from '../../assets/profile_defaultImage.svg';
 import ModifyMyInfoIcon from '../../assets/ModifyMyInfoIcon.svg';
 import Box from '../common/Box';
 import Text from '../common/Text';
@@ -23,12 +23,12 @@ const UpdateMyInfo = ({
   setMyInfoNameAndEmail,
 }: UpdateMyInfoProps) => {
   const onChangeMyInfoName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if(e.target.value.length >= 20) return;
+    if (e.target.value.length >= 20) return;
     setMyInfoNameAndEmail({ ...myInfoNameAndEmail, name: e.target.value });
   };
 
   const onChangeMyInfoEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if(e.target.value.length >= 35) return;
+    if (e.target.value.length >= 35) return;
     setMyInfoNameAndEmail({ ...myInfoNameAndEmail, email: e.target.value });
   };
 
@@ -47,7 +47,7 @@ const UpdateMyInfo = ({
       {isThereImg ? (
         <MyInfoImg src="https://images.unsplash.com/photo-1480429370139-e0132c086e2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=988&q=80" />
       ) : (
-        <InfoDefalutImg />
+        <ProfileDefaultImage />
       )}
       <Space size={7} />
       <Box>
