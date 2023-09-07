1117fff562ef18bc2c84b50dc8422cb00d5efbf4
jaeyeon kim <jakind@naver.com>
2023-09-07 17:16:47 +0900

    feat : Amazon S3 Component 추가

---
+++ backend/src/main/java/com/mapbefine/mapbefine/common/config/S3Config.java
@@ -0,0 +1,19 @@
+package com.mapbefine.mapbefine.common.config;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.AP_NORTHEAST_2)
+                .build();
+    }
+
+}
