55fe8bff28e74ec69273c33705543d6fc1f9ef04
jaeyeon kim <jakind@naver.com>
2023-09-07 19:10:05 +0900

    feat : S3 에 업로드 될 Image 의 이름을 설정해주는 ImageName 과 UploadFile 추가

---
+++ backend/src/main/java/com/mapbefine/mapbefine/s3/domain/UploadFile.java
@@ -0,0 +1,81 @@
+package com.mapbefine.mapbefine.s3.domain;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public class UploadFile implements MultipartFile {
+
+    private final String fileName;
+    private final byte[] bytes;
+
+    private UploadFile(
+            String fileName,
+            byte[] bytes
+    ) {
+        this.fileName = fileName;
+        this.bytes = bytes;
+    }
+
+    public static UploadFile of(
+            MultipartFile multipartFile
+    ) throws IOException {
+        ImageName imageName = ImageName.from(multipartFile.getOriginalFilename());
+        byte[] multipartFileBytes = multipartFile.getBytes();
+
+        return new UploadFile(imageName.getFileName(), multipartFileBytes);
+    }
+
+    @Override
+    public String getName() {
+        return fileName;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return fileName;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public long getSize() {
+        return 0;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return bytes;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new ByteArrayInputStream(bytes);
+    }
+
+    @Override
+    public Resource getResource() {
+        return MultipartFile.super
+                .getResource();
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+        FileOutputStream fileOutputStream = new FileOutputStream(dest);
+        fileOutputStream.write(bytes);
+        fileOutputStream.close();
+    }
+
+}
