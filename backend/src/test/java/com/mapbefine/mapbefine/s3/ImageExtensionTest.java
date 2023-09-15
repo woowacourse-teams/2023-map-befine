ce90de98e63bb788ae3a8ed0af258564f350e6e5
jaeyeon kim <jakind@naver.com>
2023-09-15 12:07:40 +0900

    test : imageExtension Test 작성

---
+++ backend/src/test/java/com/mapbefine/mapbefine/s3/ImageExtensionTest.java
@@ -0,0 +1,36 @@
+package com.mapbefine.mapbefine.s3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.mapbefine.mapbefine.s3.domain.ImageExtension;
+import com.mapbefine.mapbefine.s3.exception.S3Exception.S3BadRequestException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ImageExtensionTest {
+
+    @DisplayName("fromByImageFileName 를 통해 정상적으로 ImageExtension 을 생성한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"image.jpeg", "image.jpg", "image.jfif", "image.png", "image.svg"})
+    void createImageExtensionByFileName_Success(String fileName) {
+        // given when
+        String extension = ImageExtension.fromByImageFileName(fileName)
+                .getExtension();
+
+        // then
+        assertThat(fileName).contains(extension);
+    }
+
+    @DisplayName("fromByImageFileName 을 통해 존재하지 않는 확장자라면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"image.pppng", "image.jpeeg", "image.gi"})
+    void createImageExtensionByFileName_Fail(String fileName) {
+        // given when then
+        assertThatThrownBy(() -> ImageExtension.fromByImageFileName(fileName))
+                .isInstanceOf(S3BadRequestException.class);
+    }
+
+}
