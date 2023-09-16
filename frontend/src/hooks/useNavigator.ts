d2924f654c0ec513414dec9fd16becc4ae010d11
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-09-16 20:37:52 +0900

    refactor: 페이지 직접 이동 로직까지 포함하도록 수정

--- frontend/src/hooks/useNavigator.ts
+++ frontend/src/hooks/useNavigator.ts
@@ -1,14 +1,33 @@
-import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ModalContext } from '../context/ModalContext';
 
 const useNavigator = () => {
   const navigator = useNavigate();
+
+  const { openModal, closeModal } = useContext(ModalContext);
+  const { topicId } = useParams();
 
   const routePage = (url: string | -1, value?: string | number | number[]) => {
     if (typeof url === 'string') navigator(url, { state: value });
     if (url === -1) navigator(url);
   };
 
-  return { routePage };
+  return {
+    home: () => routePage('/'),
+    seeTogether: () => routePage('/see-together'),
+    addMapOrPin: () => openModal('addMapOrPin'),
+    favorite: () => routePage('/favorite'),
+    profile: () => routePage('/my-page'),
+    newTopic: () => {
+      routePage('/new-topic');
+      closeModal('addMapOrPin');
+    },
+    newPin: () => {
+      routePage(`/new-pin/${topicId}`);
+      closeModal('addMapOrPin');
+    },
+  };
 };
 
 export default useNavigator;
