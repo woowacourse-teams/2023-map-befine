8659ecde5d467634efdfaf5e4325ff166e79fe83
cpot5620 <cpot5620@gmail.com>
2023-09-18 22:45:20 +0900

    feat: LatencyRecorder Thread-safe 테스트 구현

--- backend/src/test/java/com/mapbefine/mapbefine/common/filter/LatencyRecorderTest.java
+++ backend/src/test/java/com/mapbefine/mapbefine/common/filter/LatencyRecorderTest.java
@@ -25,4 +25,24 @@ class LatencyRecorderTest {
         assertThat(latencyForSeconds).isGreaterThan(5)
                 .isLessThan(6);
     }
+
+    @Test
+    @DisplayName("다른 쓰레드에 영향을 받지 않는다.")
+    void getLatencyForSecondsThreadSafe() throws InterruptedException {
+        //given
+        Thread thread = new Thread(latencyRecorder::start);
+        thread.start();
+        Thread.sleep(3000);
+
+        latencyRecorder.start();
+        Thread.sleep(3000);
+
+        //when
+        double latencyForSeconds = latencyRecorder.getLatencyForSeconds();
+
+        //then
+        assertThat(latencyForSeconds).isGreaterThan(3)
+                .isLessThan(4);
+    }
+
 }
