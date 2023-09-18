3d54c201ab89c6dedb873c63fc6fcda471b72931
준팍(junpak) <112045553+junpakPark@users.noreply.github.com>
2023-09-18 19:46:10 +0900

    [BE] HotFix/#426 Refresh Token 중복 저장 방지 로직 수정 (#431)
    * fix: RefreshToken Payload 추가 및 CORS 완화
    
    * fix: Refresh Token Header 허용
    
    * fix: CORS 재설정 및 sameSite None
    
    * fix: refreshToken 존재 시 삭제 로직 변경

--- backend/src/main/java/com/mapbefine/mapbefine/auth/domain/token/RefreshTokenRepository.java
+++ backend/src/main/java/com/mapbefine/mapbefine/auth/domain/token/RefreshTokenRepository.java
@@ -1,11 +1,10 @@
 package com.mapbefine.mapbefine.auth.domain.token;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
 
-    Optional<RefreshToken> findByMemberId(Long memberId);
+    boolean existsByMemberId(Long memberId);
 
     void deleteByMemberId(Long memberId);
 
