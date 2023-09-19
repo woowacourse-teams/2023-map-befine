cf39b5ed4d70e60082d2dfd5cca12016e5672803
준팍(junpak) <112045553+junpakPark@users.noreply.github.com>
2023-09-19 20:32:28 +0900

    [BE] HotFix/#424 refresh token duplicated (#441)
    * fix: RefreshToken Payload 추가 및 CORS 완화
    
    * fix: Refresh Token Header 허용
    
    * fix: CORS 재설정 및 sameSite None
    
    * fix: 디버깅을 위한 에러코드 추가

--- backend/src/main/java/com/mapbefine/mapbefine/auth/exception/AuthErrorCode.java
+++ backend/src/main/java/com/mapbefine/mapbefine/auth/exception/AuthErrorCode.java
@@ -8,7 +8,8 @@ public enum AuthErrorCode {
     ILLEGAL_TOKEN("01101", "로그인에 실패하였습니다."),
     FORBIDDEN_ADMIN_ACCESS("01102", "로그인에 실패하였습니다."),
     BLOCKING_MEMBER_ACCESS("01103", "로그인에 실패하였습니다."),
-    EXPIRED_TOKEN("01104", "기간이 만료된 토큰입니다.")
+    EXPIRED_TOKEN("01104", "기간이 만료된 토큰입니다."),
+    BAD_REQUEST_TOKEN("01005", "잘못된 요청입니다.")
     ;
 
     private final String code;
