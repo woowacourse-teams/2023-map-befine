0736aa8875c4806ea84fb97d65004899d37d2420
jaeyeon kim <jakind@naver.com>
2023-09-20 22:21:20 +0900

    refactor : Illegal Image File Extension 에러 메세지 수정

--- backend/src/main/java/com/mapbefine/mapbefine/image/exception/ImageErrorCode.java
+++ backend/src/main/java/com/mapbefine/mapbefine/image/exception/ImageErrorCode.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ImageErrorCode {
 
-    ILLEGAL_IMAGE_FILE_EXTENSION("10000", "이미지 파일이 아닙니다."),
+    ILLEGAL_IMAGE_FILE_EXTENSION("10000", "지원하지 않는 이미지 파일입니다."),
     IMAGE_FILE_IS_NULL("10001", "이미지가 선택되지 않았습니다.")
     ;
 
