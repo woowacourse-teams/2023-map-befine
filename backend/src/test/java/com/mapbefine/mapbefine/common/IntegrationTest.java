0c1f72cca69b62bbabcf189f1c14a07cc5a52f5a
yoondgu <doyoungwork@gmail.com>
2023-09-25 16:09:05 +0900

    refactor: 불필요한 테스트 어노테이션 제거

--- backend/src/test/java/com/mapbefine/mapbefine/common/IntegrationTest.java
+++ backend/src/test/java/com/mapbefine/mapbefine/common/IntegrationTest.java
@@ -1,17 +1,13 @@
 package com.mapbefine.mapbefine.common;
 
 import com.mapbefine.mapbefine.DatabaseCleanup;
-import io.restassured.*;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Profile;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(locations = "classpath:application.yml")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
 
