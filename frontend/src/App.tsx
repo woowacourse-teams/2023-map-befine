371b890e181629e062b7e216073911e739b8c9d5
GC-Park <kuyt3122@naver.com>
2023-09-27 00:49:49 +0900

    refactor: 필요없는 코드 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import { useEffect } from 'react';
-import ReactGA from 'react-ga';
-import { createBrowserHistory as createHistory } from 'history';
 
 const App = () => {
-  const history = createHistory();
-  
-  useEffect(() => {
-    ReactGA.initialize('G-RRSTX6Y61Y', { debug: true });
-    history.listen((location: any) => {
-      ReactGA.set({ page: location.pathname });
-      ReactGA.pageview(location.pathname);
-    });
-  }, []);
   return <RouterProvider router={router} />;
 };
 
