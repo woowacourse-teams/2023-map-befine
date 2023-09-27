64769312cab1f7207191465720262973288565e0
ParkGeunCheol <72205402+GC-Park@users.noreply.github.com>
2023-09-27 15:23:19 +0900

    [FE] Refactor/#505 react ga4 적용 (#506)
    * chore: react-ga를 react-ga4로 변경 및 다른 패키지 업데이트
    
    * refactor: google analytics react-ga4로 변경
    
    * refactor: frontend workflow 변경

---
+++ frontend/src/utils/RouteChangeTracker.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+
+const RouteChangeTracker = () => {
+  const location = useLocation();
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
+      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+      setInitialized(true);
+    }
+  }, []);
+
+  // location 변경 감지시 pageview 이벤트 전송
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.send('pageview');
+    }
+  }, [initialized, location]);
+};
+
+export default RouteChangeTracker;
