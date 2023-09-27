9d575f5dce26abd7dfe66fba0acc04b67abe156b
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-09-27 11:27:32 +0900

    feat: patch용 함수 구현

---
+++ frontend/src/apiHooks/usePatch.ts
@@ -0,0 +1,43 @@
+import { patchApi } from '../apis/patchApi';
+import useToast from '../hooks/useToast';
+import { ContentTypeType } from '../types/Api';
+
+interface fetchPutProps {
+  url: string;
+  payload: {};
+  errorMessage: string;
+  contentType?: ContentTypeType;
+  onSuccess?: () => void;
+  isThrow?: boolean;
+}
+
+const usePatch = () => {
+  const { showToast } = useToast();
+
+  const fetchPatch = async ({
+    url,
+    payload,
+    contentType,
+    errorMessage,
+    onSuccess,
+    isThrow,
+  }: fetchPutProps) => {
+    try {
+      const responseData = await patchApi(url, payload, contentType);
+
+      if (onSuccess) {
+        onSuccess();
+      }
+
+      return responseData;
+    } catch (e) {
+      showToast('error', errorMessage);
+
+      if (isThrow) throw e;
+    }
+  };
+
+  return { fetchPatch };
+};
+
+export default usePatch;
