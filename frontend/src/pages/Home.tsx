5d5a93afc03bdf25f206a5212642d60791fbacb2
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-09-27 16:05:12 +0900

    feat: 환경변수 테스트

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -14,6 +14,7 @@ const TopicListContainer = lazy(
 );
 
 const Home = () => {
+  console.log(`${process.env.APP_URL}`, '환경변수 테스트');
   const { routingHandlers } = useNavigator();
   const { goToPopularTopics, goToLatestTopics, goToNearByMeTopics } =
     routingHandlers;
