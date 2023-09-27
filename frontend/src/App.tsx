e49bbd5b640168cfbf39ac4cc20bc35846481d13
GC-Park <kuyt3122@naver.com>
2023-09-27 15:58:00 +0900

    fix: useLocation 위치로 인한 에러 해결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import RouteChangeTracker from './utils/RouteChangeTracker';
 
 const App = () => {
-  RouteChangeTracker();
-
   return <RouterProvider router={router} />;
 };
 
--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -2,8 +2,10 @@ import { Outlet } from 'react-router-dom';
 import Layout from '../components/Layout';
 import LayoutWidthProvider from '../context/LayoutWidthContext';
 import NavbarHighlightsProvider from '../context/NavbarHighlightsContext';
+import RouteChangeTracker from '../utils/RouteChangeTracker';
 
 const RootPage = () => {
+  RouteChangeTracker();
   return (
     <>
       <LayoutWidthProvider>
