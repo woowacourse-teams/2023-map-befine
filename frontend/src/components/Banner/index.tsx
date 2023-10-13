26779fbe9baf9d2afd07b61e7d9a3d9677e648bb
semnil5202 <semnil5202@gmail.com>
2023-10-13 14:30:23 +0900

    design: 배너, 토픽카드 이미지 오류 수정
    머지 과정에서 일부 충돌 사항을 해결한다.

--- frontend/src/components/Banner/index.tsx
+++ frontend/src/components/Banner/index.tsx
@@ -4,11 +4,9 @@ import BannerItemBoongWEBP from '../../assets/banner_boong.webp';
 import BannerItemUsageWEBP from '../../assets/banner_usage.webp';
 import useNavigator from '../../hooks/useNavigator';
 import Box from '../common/Box';
-import Image from '../common/Image';
 import Swiper from '../common/Swiper';
 import Tab from '../common/Swiper/Tab';
 
-const BOONG_A_BBANG_URL = process.env.APP_URL ? '/topics/62' : '/topics/1';
 const USAGE_URL =
   'https://yoondgu.notion.site/3e5b3c98c4814aa1bd5887104fee314e?pvs=4';
 
@@ -16,7 +14,12 @@ export default function Banner() {
   const { routePage } = useNavigator();
 
   const goToBoongABbangTopic = () => {
-    routePage(BOONG_A_BBANG_URL);
+    if (process.env.APP_URL === 'https://mapbefine.kro.kr/api') {
+      routePage('/topics/1', 1);
+      return;
+    }
+
+    routePage('/topics/62', 62);
   };
 
   return (
@@ -31,7 +34,11 @@ export default function Banner() {
     >
       <Tab label="붕어빵 지도">
         <Box cursor="pointer" onClick={goToBoongABbangTopic}>
-          <BannerImage src={BannerItemBoongWEBP} alt="붕어빵 배너" />
+          <BannerImage
+            width="100%"
+            src={BannerItemBoongWEBP}
+            alt="붕어빵 배너"
+          />
         </Box>
       </Tab>
       <Tab label="사용법">
@@ -45,7 +52,7 @@ export default function Banner() {
   );
 }
 
-const BannerImage = styled(Image)`
+const BannerImage = styled.img`
   width: 100%;
   min-height: 200px;
 `;
