bef0a55a7a8101e01333cab15ac1f7a826f6fdf1
yoondgu <doyoungwork@gmail.com>
2023-11-01 22:51:33 +0900

    test: JPA 쿼리 호출과 JdbcTemplate 쿼리 호출 분리

--- backend/src/main/java/com/mapbefine/mapbefine/common/repository/PinBatchRepositoryCustomImpl.java
+++ backend/src/main/java/com/mapbefine/mapbefine/common/repository/PinBatchRepositoryCustomImpl.java
@@ -3,7 +3,6 @@ package com.mapbefine.mapbefine.common.repository;
 import com.mapbefine.mapbefine.pin.domain.Pin;
 import com.mapbefine.mapbefine.pin.domain.PinImage;
 import com.mapbefine.mapbefine.topic.domain.Topic;
-import jakarta.persistence.EntityManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
@@ -16,38 +15,35 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Repository
 public class PinBatchRepositoryCustomImpl implements PinBatchRepositoryCustom {
 
-    private final EntityManager entityManager;
     private final JdbcTemplate jdbcTemplate;
 
-    public PinBatchRepositoryCustomImpl(EntityManager entityManager, JdbcTemplate jdbcTemplate) {
-        this.entityManager = entityManager;
+    public PinBatchRepositoryCustomImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    @Transactional
     public int[] saveAllToTopic(Topic topicForCopy, List<Pin> originalPins) {
         int[] rowCount = batchUpdatePins(topicForCopy, originalPins);
         Long firstIdFromBatch = jdbcTemplate.queryForObject("SELECT last_insert_id()", Long.class);
         validateId(firstIdFromBatch);
-        List<PinImageDTO> pinImageDTOsToBatches = createPinImageDTOsToBatch(originalPins, rowCount, firstIdFromBatch);
+        List<PinImageInsertDto> pinImageInsertDtos = createPinImageDTOsToBatch(originalPins, rowCount,
+                firstIdFromBatch);
 
-        return batchUpdatePinImages(pinImageDTOsToBatches);
+        if (pinImageInsertDtos.isEmpty()) {
+            return rowCount;
+        }
+        return batchUpdatePinImages(pinImageInsertDtos);
     }
 
     private int[] batchUpdatePins(
             Topic topicForCopy,
             List<Pin> originalPins
     ) {
-        LocalDateTime createdAt = LocalDateTime.now();
-        topicForCopy.increasePinCount(originalPins.size());
-        topicForCopy.updateLastPinUpdatedAt(createdAt);
-        entityManager.flush();
+        LocalDateTime createdAt = topicForCopy.getLastPinUpdatedAt();
 
         String bulkInsertSql = "INSERT INTO pin ("
                 + "name, description, member_id, topic_id, location_id,"
@@ -91,31 +87,31 @@ public class PinBatchRepositoryCustomImpl implements PinBatchRepositoryCustom {
         }
     }
 
-    private List<PinImageDTO> createPinImageDTOsToBatch(
+    private List<PinImageInsertDto> createPinImageDTOsToBatch(
             List<Pin> originalPins,
             int[] rowCount,
             Long firstIdFromBatch
     ) {
         return IntStream.range(0, originalPins.size())
-                .filter(index -> rowCount[index] != 0)
+                .filter(index -> rowCount[index] != -3)
                 .mapToObj(index -> {
                     Pin pin = originalPins.get(index);
-                    return PinImageDTO.of(pin.getPinImages(), firstIdFromBatch + index);
+                    return PinImageInsertDto.of(pin.getPinImages(), firstIdFromBatch + index);
                 }).flatMap(Collection::stream)
                 .toList();
     }
 
-    private int[] batchUpdatePinImages(List<PinImageDTO> pinImages) {
-        String bulkInsertSql = "INSERT INTO pin_image ("
-                + "image_url, pin_id) "
-                + "VALUES ("
-                + "?, ?)";
+    private int[] batchUpdatePinImages(List<PinImageInsertDto> pinImages) {
+        String bulkInsertSql = "INSERT INTO pin_image "
+                + "(image_url, pin_id) "
+                + "VALUES "
+                + "(?, ?)";
         log.debug("[Query] bulk insert size {} : {}", pinImages.size(), bulkInsertSql);
 
         return jdbcTemplate.batchUpdate(bulkInsertSql, new BatchPreparedStatementSetter() {
             @Override
             public void setValues(PreparedStatement ps, int i) throws SQLException {
-                PinImageDTO pinImage = pinImages.get(i);
+                PinImageInsertDto pinImage = pinImages.get(i);
                 ps.setString(1, pinImage.getImageUrl());
                 ps.setLong(2, pinImage.getPinId());
                 log.trace("[Parameter Binding] {} : imageUrl={}, pinImage={} ",
@@ -129,30 +125,29 @@ public class PinBatchRepositoryCustomImpl implements PinBatchRepositoryCustom {
         });
     }
 
-    // TODO 네이밍 고민해보기
-    private static class PinImageDTO {
+    private static class PinImageInsertDto {
 
         private final String imageUrl;
         private final Long pinId;
         private final boolean isDeleted;
 
-        private PinImageDTO(String imageUrl, Long pinId, boolean isDeleted) {
+        private PinImageInsertDto(String imageUrl, Long pinId, boolean isDeleted) {
             this.imageUrl = imageUrl;
             this.pinId = pinId;
             this.isDeleted = isDeleted;
         }
 
-        public static PinImageDTO of(PinImage pinImage, Long pinId) {
-            return new PinImageDTO(
+        public static PinImageInsertDto of(PinImage pinImage, Long pinId) {
+            return new PinImageInsertDto(
                     pinImage.getImageUrl(),
                     pinId,
                     pinImage.isDeleted()
             );
         }
 
-        private static List<PinImageDTO> of(List<PinImage> pinImages, Long pinId) {
+        private static List<PinImageInsertDto> of(List<PinImage> pinImages, Long pinId) {
             return pinImages.stream()
-                    .map(pinImage -> PinImageDTO.of(pinImage, pinId))
+                    .map(pinImage -> PinImageInsertDto.of(pinImage, pinId))
                     .toList();
         }
 
