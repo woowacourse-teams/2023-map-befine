701cbbb2a5dab307c53b76079aacb15df4f51dc0
jaeyeon kim <jakind@naver.com>
2023-11-06 17:15:06 +0900

    test : Cluster Test 추가

---
+++ backend/src/test/java/com/mapbefine/mapbefine/topic/domain/ClusterTest.java
@@ -0,0 +1,45 @@
+package com.mapbefine.mapbefine.topic.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.mapbefine.mapbefine.location.LocationFixture;
+import com.mapbefine.mapbefine.location.domain.Location;
+import com.mapbefine.mapbefine.member.MemberFixture;
+import com.mapbefine.mapbefine.member.domain.Member;
+import com.mapbefine.mapbefine.member.domain.Role;
+import com.mapbefine.mapbefine.pin.PinFixture;
+import com.mapbefine.mapbefine.pin.domain.Pin;
+import com.mapbefine.mapbefine.topic.TopicFixture;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ClusterTest {
+
+    @Test
+    @DisplayName("대표 핀과, 해당 좌표에 묶인 핀들의 집합을 받으면 정상적으로 생성된다.")
+    void createCluster() {
+        // given
+        Member member = MemberFixture.create("member", "member@naver.com", Role.USER);
+        Topic topic = TopicFixture.createByName("topic", member);
+        Location firstLocation = LocationFixture.createByCoordinate(36, 124);
+        Location secondLocation = LocationFixture.createByCoordinate(36, 124.1);
+        List<Pin> pins = List.of(
+                PinFixture.create(firstLocation, topic, member),
+                PinFixture.create(secondLocation, topic, member)
+        );
+
+        // when
+        Cluster cluster = Cluster.from(pins.get(0), pins);
+
+        // then
+        assertAll(
+                () -> assertThat(cluster.getLatitude()).isEqualTo(36),
+                () -> assertThat(cluster.getLongitude()).isEqualTo(124),
+                () -> assertThat(cluster.getPins()).usingRecursiveComparison()
+                        .isEqualTo(pins)
+        );
+    }
+
+}
