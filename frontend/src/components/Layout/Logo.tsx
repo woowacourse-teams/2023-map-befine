eb2d6c3373cd30dce5d1e88557263038cc08d678
semnil5202 <semnil5202@gmail.com>
2023-11-23 16:31:16 +0900

    refactor: Logo aria-label 수정

--- frontend/src/components/Layout/Logo.tsx
+++ frontend/src/components/Layout/Logo.tsx
@@ -17,8 +17,8 @@ function Logo() {
       onKeyDown={onElementKeyDown}
       ref={elementRef}
       onClick={goToHome}
-      aria-label="괜찮을지도 로고 및 홈으로 이동 버튼"
       tabIndex={0}
+      aria-label="괜찮을지도 로고 및 홈으로 이동하기"
     >
       <LogoImage />
     </Box>
