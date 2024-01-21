258cbf875fe7bf158e02c9e47c0c11e1da775055
afds4567 <33995840+afds4567@users.noreply.github.com>
2024-01-21 23:02:15 +0900

    Refactor : Remove unused code and update import path

--- frontend/src/apis/Patrick/index.ts
+++
@@ -1,6 +0,0 @@
-import { http } from './http';
-import { TopicCardProps } from '../../types/Topic';
-
-export const getProfile = () => {
-  return http.get<TopicCardProps[] | null>("/members/my/topics");
-};
