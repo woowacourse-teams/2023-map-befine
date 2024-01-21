7827d0ef669a037c0bb16cae2edb76317fa094f7
afds4567 <33995840+afds4567@users.noreply.github.com>
2024-01-21 23:02:24 +0900

    Refactor : Update default topic and profile images

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -5,9 +5,9 @@ export const SIDEBAR = '372px';
 export const LAYOUT_PADDING = '40px';
 
 export const DEFAULT_TOPIC_IMAGE =
-  'https://dr702blqc4x5d.cloudfront.net/2023-map-be-fine/icon/topic_defaultImage.svg';
+  'https://velog.velcdn.com/images/semnil5202/post/37f3bcb9-0b07-4100-85f6-f1d5ad037c14/image.svg';
 export const DEFAULT_PROFILE_IMAGE =
-  'https://dr702blqc4x5d.cloudfront.net/2023-map-be-fine/icon/profile_defaultImage.svg';
+  'https://avatars.githubusercontent.com/u/103165859?v=4';
 
 export const DEFAULT_PROD_URL =
   process.env.APP_URL || 'https://mapbefine.com/api';
